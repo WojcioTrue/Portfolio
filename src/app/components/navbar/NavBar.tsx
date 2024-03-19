d5207c6e796f4f1467c122480d5e78afc7539bc1
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-03-19 21:56:20 +0100

    added display rules for navbar component

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -2,15 +2,32 @@ import Image from "next/image"
 
 const NavBar = () => {
     return (
-        <nav className="navbar bg-slate-400 p-3">
-            <div className="navbar__brand bg-red-500 w-fit">
+        <nav className="navbar bg-slate-400 p-3 md:flex justify-between">
+            <a className="navbar__brand bg-red-500 w-fit p-1 inline-block">
                 <Image
                     src="/template_logo.svg"
                     alt="logo missing"
                     width="25"
                     height="25"
+                    className="inline"
                 />
-            <p>Portfolio_MR.Crabbbss</p>
+                <p className="pl-2 inline">Portfolio</p>
+            </a>
+            <button
+                className="bg-yellow-400 p-1 md:hidden inline-block float-right"
+                type="button"
+                data-target="#dropdown__navbar"
+                aria-controls="dropdown__navbar"
+                aria-expanded="false"
+                aria-label="Toggle navbar"
+            >X</button>
+            <div className="navbar__collapse bg-fuchsia-500 p-1" id="dropdown__navbar">
+                <ul className="md:flex">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                </ul>
             </div>
         </nav>
     )
