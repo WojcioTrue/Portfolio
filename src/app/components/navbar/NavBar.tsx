729ecfda237758dcebc8622a1bc15c23cd00242a
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-06-20 18:39:57 +0200

    Added animation for fixed menu while scrolling

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -4,11 +4,28 @@ import NavBarList from "./NavBarList"
 import { useEffect, useState, createContext } from "react"
 import MobileNavBar from "./mobileNavbar/MobileNavBar"
 import BackDrop from "./mobileNavbar/backdrop/BackDrop"
+import navBarStyles from "./NavBar.module.scss"
 
 export const DisplayMenuContext = createContext<{ displayMenu: boolean, setDisplayMenu: Dispatch<SetStateAction<boolean>> }>({ displayMenu: false, setDisplayMenu: () => { } })
 
 const NavBar = () => {
+    // state for context
     const [displayMenu, setDisplayMenu] = useState<boolean>(false)
+    //state for changing menu to fixed
+    const [styleFixed, setStyleFixed] = useState(false)
+
+    // check if window passed breakpoint while scrolling
+    const scrolledWindow = () => {
+        console.log(navBarStyles)
+        const scrolled = window.scrollY > 100 ? true : false
+        setStyleFixed(scrolled)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrolledWindow)
+        // cleanup
+        return () => window.removeEventListener('scroll', scrolledWindow)
+    }, [])
 
     useEffect(() => {
         displayMenu ? document.body.classList.add('overflow-y-hidden') : document.body.classList.remove('overflow-y-hidden')
@@ -16,7 +33,7 @@ const NavBar = () => {
 
     return (
         <DisplayMenuContext.Provider value={{ displayMenu, setDisplayMenu }}>
-            <nav className="navbar bg-slate-400 w-full py-6 px-4">
+            <nav className={`${styleFixed && navBarStyles.navchange} ${styleFixed && 'fixed'} bg-slate-400 navbar w-full py-6 px-4 `}>
                 <span className="flex justify-between items-stretch flex-col md:flex-row max-w-7xl m-auto h-auto">
                     <div className="flex justify-between w-full items-center">
                         <a className="navbar__brand flex" href="#">
