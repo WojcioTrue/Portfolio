d59587e5881fcdb3f8134147220c238159895567
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-07-11 22:04:23 +0200

    Fixed position of animated background Component

--- src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
@@ -6,15 +6,15 @@ const NavBarAnimatedBg = () => {
     const { position } = navBarItemPosition
 
     return (
-        <div className="absolute top-0 left-0">
-            {/* <div 
+        <div className="nav-bar-animated-bg absolute h-[30px] w-full top-0 left-0 overflow-hidden">
+            <span 
             className={`right-element bg-cyan-300 w-[400px] h-7 absolute top-0`} 
             style={{ right: `${position.right}px` }}>
-            </div> */}
-            <div
-            className={`left-element bg-red-600 w-[400px] h-7 absolute top-0`}
+            </span>
+            <span
+            className={`transition-all left-element bg-red-600 w-[400px] h-7 absolute top-0`}
             style={{ left: `${position.left}px` }}>
-            </div>
+            </span>
         </div>
 
     )
