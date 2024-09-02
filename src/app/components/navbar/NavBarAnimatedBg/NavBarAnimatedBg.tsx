6b3ed8616dde48e8382df6a3fe07a829e19ddb4f
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-02 20:08:15 +0200

    Changed transition duration to 75ms

--- src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
@@ -8,11 +8,11 @@ const NavBarAnimatedBg = () => {
     return (
         <div className="nav-bar-animated-bg absolute h-full w-full top-0 left-0 overflow-hidden z-10">
             <span 
-            className={`right-element bg-cyan-300 w-full h-full absolute top-0 active:text-white`} 
+            className={`transition-all duration-75 right-element bg-cyan-300 w-full h-full absolute top-0 active:text-white`} 
             style={{ right: `${position.right}px` }}>
             </span>
             <span
-            className={`transition-all left-element bg-red-600 w-full h-full absolute top-0`}
+            className={`transition-all duration-75 left-element bg-red-600 w-full h-full absolute top-0`}
             style={{ left: `${position.left}px` }}>
             </span>
         </div>
