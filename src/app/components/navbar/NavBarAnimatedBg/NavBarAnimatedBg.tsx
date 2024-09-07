51dd79631739bc12f5293b3efbef97338c1913ce
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-07 14:07:03 +0200

    Added animation for moving from top

--- src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { NavBarContext } from "../NavBar"
+import NavBarAnimatedStyles from "./NavBarAnimatedBg.module.scss"
 
 const NavBarAnimatedBg = () => {
     const { navBarItemPosition, navBarItems } = useContext(NavBarContext)
@@ -7,18 +8,24 @@ const NavBarAnimatedBg = () => {
     const [isActive, setIsActive] = useState(false)
 
     useEffect(() => {
+        console.log(NavBarAnimatedStyles)
+    }, [])
+
+    useEffect(() => {
         const isActive = navBarItems.listElements.some((x) => Boolean(x.active) === true)
         setIsActive(isActive)
     }, [navBarItems.listElements])
-
+    // 
     return (
         <div className={"static transition-all nav-bar-animated-bg h-full w-full left-0 overflow-hidden"}>
             <span
-                className={`transition-all duration-100 right-element bg-cyan-300 w-[200%] h-full absolute ${!isActive ? "top-[-100%]" : "top-[0%]"}`}
-                style={{ right: `${position.right -1}px` }}>
+                className={`right-element bg-cyan-300 w-[200%] h-full absolute  
+                    ${isActive ? NavBarAnimatedStyles.animation : NavBarAnimatedStyles.click}`}
+                style={{ right: `${position.right - 1}px` }}>
             </span>
             <span
-                className={`transition-all duration-100 left-element bg-red-600 w-[200%] h-full absolute ${!isActive ? "top-[-100%]" : "top-[0%]"}`}
+                className={` left-element bg-red-600 w-[200%] h-full absolute 
+                    ${isActive ? NavBarAnimatedStyles.animation : NavBarAnimatedStyles.click}`}
                 style={{ left: `${position.left}px` }}>
             </span>
         </div>
