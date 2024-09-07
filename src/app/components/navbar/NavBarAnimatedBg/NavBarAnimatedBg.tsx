e28972111507b7983349c5bde89a7787da07b380
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-07 15:07:58 +0200

    Fixed animation for moving background

--- src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/components/navbar/NavBarAnimatedBg/NavBarAnimatedBg.tsx
@@ -15,17 +15,22 @@ const NavBarAnimatedBg = () => {
         const isActive = navBarItems.listElements.some((x) => Boolean(x.active) === true)
         setIsActive(isActive)
     }, [navBarItems.listElements])
+
     // 
+
     return (
-        <div className={"static transition-all nav-bar-animated-bg h-full w-full left-0 overflow-hidden"}>
+        <div className={`static h-full w-full left-0 overflow-hidden`}>
             <span
-                className={`right-element bg-cyan-300 w-[200%] h-full absolute  
-                    ${isActive ? NavBarAnimatedStyles.animation : NavBarAnimatedStyles.click}`}
+                className={` bg-cyan-300 w-[200%] h-full absolute 
+                    ${NavBarAnimatedStyles.right_animation}
+                    ${isActive ? NavBarAnimatedStyles.top_animation : NavBarAnimatedStyles.click}
+                `}
                 style={{ right: `${position.right - 1}px` }}>
             </span>
             <span
-                className={` left-element bg-red-600 w-[200%] h-full absolute 
-                    ${isActive ? NavBarAnimatedStyles.animation : NavBarAnimatedStyles.click}`}
+                className={`bg-red-600 w-[200%] h-full absolute ${NavBarAnimatedStyles.left_animation}
+                ${isActive ? NavBarAnimatedStyles.top_animation : NavBarAnimatedStyles.click}
+                `}
                 style={{ left: `${position.left}px` }}>
             </span>
         </div>
