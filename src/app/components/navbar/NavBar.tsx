0eb21422848378225c6d2e5894ef60c64b2d3c77
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-07 17:13:38 +0200

    added dependency to useEffect hook

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -94,7 +94,7 @@ const NavBar = () => {
                 right: rightValue
             })
         }
-    }, [])
+    }, [navBarItems.listElements])
 
     return (
         <NavBarContext.Provider value={
