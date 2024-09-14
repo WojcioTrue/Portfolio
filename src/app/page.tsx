de0669b85a102661797a4203629e6b6fd3a26f07
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-14 11:33:36 +0200

    Created banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/navbar/NavBar";
+import Banner from "./components/banner/Banner";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Banner />
     </>
   );
 }
