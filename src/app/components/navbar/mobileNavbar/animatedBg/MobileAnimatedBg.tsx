2cf7908be1f017c75a282d3f35257385dba7f677
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-28 12:16:21 +0200

    Added context for MobileAnimatedBg

--- src/app/components/navbar/mobileNavbar/animatedBg/MobileAnimatedBg.tsx
+++ src/app/components/navbar/mobileNavbar/animatedBg/MobileAnimatedBg.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Bottom from './Bottom'
 import Top from './Top'
+import { NavBarContext } from '../../NavBar'
 
 const MobileAnimatedBg = () => {
+  const { toogleMobileNav } = useContext(NavBarContext)
+  const { displayMenu, setDisplayMenu } = toogleMobileNav
+
+  
+
   return (
-    <div className='absolute top-0 h-[100%] w-full z-[-10]'>
-        <Top/>
-        <Bottom/>
+    <div className='absolute top-0 h-[100%] w-full z-[-10]'
+      style={{ left: `95%` }}
+    >
+      <Top />
+      <Bottom />
     </div>
   )
 }
