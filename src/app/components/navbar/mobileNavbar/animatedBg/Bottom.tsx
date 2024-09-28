e69e5c949c6304abc0997522460bbde32b83acd0
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-09-28 11:23:31 +0200

    postion context added to top and bottom component

--- src/app/components/navbar/mobileNavbar/animatedBg/Bottom.tsx
+++ src/app/components/navbar/mobileNavbar/animatedBg/Bottom.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavBarContext } from '../../NavBar'
+
 
 const Bottom = () => {
+  const { navBarItemPosition } = useContext(NavBarContext)
+  const { position } = navBarItemPosition
+
   return (
-    <div className='relative bg-blue-300 h-[100px] z-[-10]'></div>
+    <div className='relative bg-blue-300 h-[100px] z-[-10]'>
+      {`postion top: ${position.bottom}`}
+    </div>
   )
 }
 
--- src/app/components/navbar/mobileNavbar/animatedBg/Top.tsx
+++ src/app/components/navbar/mobileNavbar/animatedBg/Top.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavBarContext } from '../../NavBar'
 
 const Top = () => {
+  const { navBarItemPosition } = useContext(NavBarContext)
+  const { position } = navBarItemPosition
+
   return (
-    <div className='relative bg-green-300 h-[100px] z-[-10]'></div>
+    <div className='relative bg-green-300 h-[100px] z-[-10]'>
+      {`postion top: ${position.top}`}
+    </div>
   )
 }
 
