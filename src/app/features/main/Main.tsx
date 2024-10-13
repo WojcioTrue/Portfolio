66fc9e9cd93b2d272cff8836bfe9e557b02e9905
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-13 15:55:49 +0200

    installed react icons

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -8,16 +8,16 @@ const Main = () => {
   return (
     <div className="relative h-full bg-slate-100">
       <div className='h-full w-full max-w-[1200px] px-8 flex justify-center items-center ml:justify-start m-auto'>
-        <div className='max-w-[500px] flex flex-col flex-nowrap items-center ml:items-start'>
+        <div className='max-w-[600px] flex flex-col flex-nowrap items-center ml:items-start'>
           <h1 className={`${font.className} 
       text-[42px] 
       sm:text-[50px] 
       md:text-[60px] 
       xl:text-[68px] 
       2xl:text-[72px]
-      text-center`}>Wojcio_True</h1>
-          <p className="text-[16px] sm:text-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-          <Button text={'banger'} />
+      text-center mb-6`}>Wojcio_True</h1>
+          <p className="text-[16px] mb-6 sm:text-[24px] text-center ml:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <Button text={'Read more '} />
         </div>
       </div>
 
--- src/app/shared_components/Button.tsx
+++ src/app/shared_components/Button.tsx
@@ -6,7 +6,7 @@ type Button = {
 
 const Button = ({text}: Button) => {
   return (
-    <button className='text-[16px] bg-slate-400'>{text}</button>
+    <button className='text-[16px] bg-slate-400 py-[12px] px-[24px] rounded-[50px] text-white bg-gradient-to-tr from-indigo-700 to-pink-500'>{text}</button>
   )
 }
 
