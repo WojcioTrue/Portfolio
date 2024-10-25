fb6a56a234b95fc6384dd8dea28cb45975f782cc
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-25 22:46:35 +0200

    Added animation for text and button in Main component

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -2,23 +2,77 @@
 import React from 'react'
 import { mabryProBold, mabryProRegular } from '@/app/assets/fonts/mabry_pro/MabryPro'
 import { FaArrowDown } from "react-icons/fa";
-// import { motion } from 'framer-motion';
-import * as motion from "framer-motion/client"
+import { motion } from 'framer-motion';
 
 const Main = () => {
   return (
     <div className="relative h-full bg-gradient-to-br from-my-lightBackground from-10% via-white">
       <div className='h-full w-full max-w-[1200px] px-8 flex justify-center items-center ml:justify-start m-auto'>
-        <div className='max-w-[600px] flex flex-col flex-nowrap items-center ml:items-start bg-white bg-opacity-20 px-10 py-8 shadow-myshadow rounded-xl'>
-          <h1 className={`${mabryProBold.className} 
+        <motion.div
+          layout
+          initial={{
+            opacity: 0,
+            translateX: '-10px'
+          }}
+          animate={{
+            opacity: 1,
+            translateX: '0px'
+          }}
+          transition={{
+            duration: 0.3,
+            type: "Tween"
+          }}
+          className='max-w-[600px] flex flex-col flex-nowrap items-center ml:items-start bg-white bg-opacity-20 px-10 py-8 shadow-myshadow rounded-xl'>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              translateX: '-10px'
+            }}
+            animate={{
+              opacity: 1,
+              translateX: '0px'
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.2,
+              type: "Tween"
+            }}
+            className={`${mabryProBold.className} 
       text-[42px] 
       sm:text-[50px] 
       md:text-[60px] 
       xl:text-[68px] 
       2xl:text-[72px]
-      text-center mb-4`}>Wojcio_True</h1>
-          <p className={`text-[16px] text-my-gray mb-4 sm:text-[24px] text-center ml:text-left ${mabryProRegular.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+      text-center mb-4`}>Wojcio_True</motion.h1>
+          <motion.p
+            initial={{
+              opacity: 0,
+              translateX: '-10px'
+            }}
+            animate={{
+              opacity: 1,
+              translateX: '0px'
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.25,
+              type: "Tween"
+            }} className={`text-[16px] text-my-gray mb-4 sm:text-[24px] text-center ml:text-left ${mabryProRegular.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</motion.p>
+
           <motion.button
+            initial={{
+              opacity: 0,
+              translateX: '-10px'
+            }}
+            animate={{
+              opacity: 1,
+              translateX: '0px'
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.40,
+              type: "Tween"
+            }}
             whileHover={{
               scale: 1.1,
               transition: { duration: 0.2 },
@@ -53,7 +107,7 @@ const Main = () => {
                 }}
                 animate={{
                   scale: [1, 2, 3],
-                  opacity: [0,0.4, 0],
+                  opacity: [0, 0.4, 0],
                 }}
                 transition={{
                   duration: 2,
@@ -65,7 +119,7 @@ const Main = () => {
               </motion.div>
             </span>
           </motion.button>
-        </div>
+        </motion.div>
       </div>
 
     </div>
