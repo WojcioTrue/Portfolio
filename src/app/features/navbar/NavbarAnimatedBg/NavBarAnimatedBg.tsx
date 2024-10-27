8af286f459bdf8896ce0f665d6d4da61c5e2defc
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-27 18:35:44 +0100

    Added border radius and changed background color for navbarAnimatedBg

--- src/app/features/navbar/NavbarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/features/navbar/NavbarAnimatedBg/NavBarAnimatedBg.tsx
@@ -15,14 +15,14 @@ const NavBarAnimatedBg = () => {
     return (
         <div className={`static h-full w-full left-0 overflow-hidden`}>
             <span
-                className={` bg-cyan-300 w-[200%] h-full absolute 
+                className={`bg-white rounded-br-xl w-[200%] h-full absolute 
                     ${NavBarAnimatedStyles.right_animation}
                     ${isActive ? NavBarAnimatedStyles.top_animation_on : NavBarAnimatedStyles.top_animation_off}
                 `}
                 style={{ right: `${position.right}px` }}>
             </span>
             <span
-                className={`bg-red-600 w-[200%] h-full absolute ${NavBarAnimatedStyles.left_animation}
+                className={`bg-white rounded-bl-xl w-[200%] h-full absolute ${NavBarAnimatedStyles.left_animation}
                 ${isActive ? NavBarAnimatedStyles.top_animation_on : NavBarAnimatedStyles.top_animation_off}
                 `}
                 style={{ left: `${position.left}px` }}>
