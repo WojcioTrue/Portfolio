8eadc976cdaf8df32d80f66008ab5cf6f7bf9110
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-27 18:53:18 +0100

    Changed opacity of navbar

--- src/app/features/navbar/NavbarAnimatedBg/NavBarAnimatedBg.tsx
+++ src/app/features/navbar/NavbarAnimatedBg/NavBarAnimatedBg.tsx
@@ -15,14 +15,14 @@ const NavBarAnimatedBg = () => {
     return (
         <div className={`static h-full w-full left-0 overflow-hidden`}>
             <span
-                className={`bg-white rounded-br-xl w-[200%] h-[95%] absolute 
+                className={`bg-white opacity-85 rounded-br-xl w-[200%] h-[calc(100%_-_5px)] absolute 
                     ${NavBarAnimatedStyles.right_animation}
                     ${isActive ? NavBarAnimatedStyles.top_animation_on : NavBarAnimatedStyles.top_animation_off}
                 `}
                 style={{ right: `${position.right}px` }}>
             </span>
             <span
-                className={`bg-white rounded-bl-xl w-[200%] h-[95%] absolute ${NavBarAnimatedStyles.left_animation}
+                className={`bg-white opacity-85 rounded-bl-xl w-[200%] h-[calc(100%_-_5px)] absolute ${NavBarAnimatedStyles.left_animation}
                 ${isActive ? NavBarAnimatedStyles.top_animation_on : NavBarAnimatedStyles.top_animation_off}
                 `}
                 style={{ left: `${position.left}px` }}>
