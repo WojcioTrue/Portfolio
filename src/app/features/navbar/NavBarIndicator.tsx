a69a8326f9f6415636536feaa34a519de374398f
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-28 20:40:19 +0100

    using useEffect hook with getBoundingClientRect() method to get position for active indicator

--- src/app/features/navbar/NavBarIndicator.tsx
+++ src/app/features/navbar/NavBarIndicator.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBarStyles from './NavBar.module.scss'
 
 type NavBarIndicator = {
+    text: string,
     isActive: boolean
 }
 
-const NavBarIndicator = ({isActive} : NavBarIndicator) => {
+const NavBarIndicator = ({isActive, text} : NavBarIndicator) => {
+    const [position, setPosition] = useState([0,0])
+
+    useEffect(() => {
+        const indicatorPosition = document.getElementById(`desktop_indicator_${text}`)!.getBoundingClientRect()
+
+
+        const verticalMidPosition = (indicatorPosition.top + indicatorPosition.height/2).toFixed(0)
+
+        const horizontalMidPosition = (indicatorPosition.left + indicatorPosition.width/2).toFixed(0)
+
+
+    },[text])
+
   return (
-    <span className='relative h-[30px] flex items-center justify-start'>
-        <h2 className={`text-red-700 ${!isActive ? 'opacity-0' : 'opacity-55'}`}>X</h2>
+    <span id={`desktop_indicator_${text}`} className='relative h-[30px] w-[30px] flex items-center justify-start'>
+        
     </span>
 )
 }
