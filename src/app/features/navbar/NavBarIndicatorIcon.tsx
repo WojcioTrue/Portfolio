58260f7136fab83df659ca6092fbbcfb638d9134
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-30 18:39:15 +0100

    Created shape of indicator

--- src/app/features/navbar/NavBarIndicatorIcon.tsx
+++ src/app/features/navbar/NavBarIndicatorIcon.tsx
@@ -6,11 +6,15 @@ const NavBarIndicatorIcon = () => {
   const { desktopIPosition } = indicatorDesktop
 
   return (
-    <div className={`h-[15px] w-[15px] bg-my-purple absolute rounded-xl translate-x-1/2 translate-y-1/2 transition-all duration-500`} 
-    style={{
-      top: `${desktopIPosition.verticalMid}px`,
-      left: `${desktopIPosition.horizontalMid + 5}px`
-    }}></div>
+    <div className={`h-[30px] w-[30px] absolute`}
+      style={{
+        top: `${desktopIPosition.verticalMid}px`,
+        left: `${desktopIPosition.horizontalMid}px`
+      }}>
+      <span className='absolute w-[25px] h-[25px] rounded-2xl bg-my-purple opacity-25 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'></span>
+      <span className='absolute w-[15px] h-[15px] rounded-2xl bg-my-purple opacity-55 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 '></span>
+      <span className='absolute w-[5px] h-[5px] rounded-2xl bg-my-purple top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 '></span>
+    </div>
   )
 }
 
