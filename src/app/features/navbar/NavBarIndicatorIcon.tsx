680816ea73cd7ab37817a7007290fdda7dece4e2
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-10-30 19:05:26 +0100

    Styled indicator

--- src/app/features/navbar/NavBarIndicatorIcon.tsx
+++ src/app/features/navbar/NavBarIndicatorIcon.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { NavBarContext } from './NavBar'
+import { motion } from 'framer-motion'
 
 const NavBarIndicatorIcon = () => {
   const { indicatorDesktop } = useContext(NavBarContext)
@@ -11,9 +12,53 @@ const NavBarIndicatorIcon = () => {
         top: `${desktopIPosition.verticalMid}px`,
         left: `${desktopIPosition.horizontalMid}px`
       }}>
-      <span className='absolute w-[25px] h-[25px] rounded-2xl bg-my-purple opacity-25 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'></span>
-      <span className='absolute w-[15px] h-[15px] rounded-2xl bg-my-purple opacity-55 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 '></span>
-      <span className='absolute w-[5px] h-[5px] rounded-2xl bg-my-purple top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 '></span>
+      <motion.span
+        className='absolute w-[25px] h-[25px] rounded-2xl bg-my-purple top-[50%] left-[50%]'
+        initial={{
+          translateY: '-50%',
+          translateX: '-50%',
+          opacity: 0
+        }}
+        animate={{
+          scale: [1,1.3,1.6],
+          opacity: [0, 0.3, 0]
+        }}
+        transition={{
+          duration: 3,
+          ease: "linear",
+          repeat: Infinity,
+        }}
+      ></motion.span>
+      <motion.span className='absolute w-[20px] h-[20px] rounded-2xl bg-my-purple top-[50%] left-[50%]' 
+      initial={{
+        translateY: '-50%',
+        translateX: '-50%',
+        opacity: '40%'
+      }}
+        animate={{
+          scale: [1.2, 0.7, 1.2],
+        }}
+        transition={{
+          duration: 3,
+          ease: "easeOut",
+          times: [0, 0.5, 1],
+          repeat: Infinity,
+        }}></motion.span>
+      <motion.span className='absolute w-[10px] h-[10px] rounded-2xl bg-my-purple opacity-55 top-[50%] left-[50%]' 
+      initial={{
+        translateY: '-50%',
+        translateX: '-50%',
+        opacity: '70%'
+      }}
+        animate={{
+          scale: [1.2, 0.7, 1.2],
+        }}
+        transition={{
+          duration: 3,
+          ease: "easeOut",
+          times: [0, 0.5, 1],
+          repeat: Infinity,
+        }}></motion.span>
     </div>
   )
 }
