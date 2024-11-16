7ea73ca88f7cd3c55dd3b1261a1663aadddfc3c2
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-11-16 16:38:05 +0100

    Making more readable code

--- src/app/features/navbar/navBarHooks/useDefaultBackground.tsx
+++ src/app/features/navbar/navBarHooks/useDefaultBackground.tsx
@@ -34,13 +34,16 @@ export const clickChangeCenter = ({ event, position, setPosition }: ClickChangeT
 }
 
 export const getInitialPosition = ({ id, desktopPosition }: GetInitialPositionType) => {
+    
     const element = document.getElementById(id)!;
     const elementWidth = Math.round(Number(((element as HTMLLIElement).clientWidth)));
-    // offset left element with 'left' property
+
     const leftValue = Number((element as HTMLLIElement).getBoundingClientRect().left.toFixed(0)) + elementWidth;
-    // offset element with 'right' property
+
     const rightValue = (document.body.clientWidth - (Number((element as HTMLLIElement).getBoundingClientRect().right.toFixed(0)) - elementWidth))
+
     const topValue = Number(element.getBoundingClientRect().top.toFixed(0))
+
     const bottomValue = Number(element.getBoundingClientRect().bottom.toFixed(0))
 
     return { ...desktopPosition, left: leftValue, right: rightValue }
