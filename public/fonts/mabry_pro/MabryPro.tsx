05640da1cbb262c5d1cc6c14785c52f694168b85
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-11-30 14:34:39 +0100

    Testing changed file directory for fonts

--- public/fonts/mabry_pro/MabryPro.tsx
+++ public/fonts/mabry_pro/MabryPro.tsx
@@ -1,17 +1,14 @@
 import localFont from 'next/font/local'
 
+const mabryProBold = localFont({ src: './MabryPro-bold.woff2' })
+const mabryProMedium = localFont({ src: './MabryPro-Medium.woff2' })
+const mabryProBlack = localFont({ src: './MabryPro-Black.woff2' })
+const mabryProLightItalic = localFont({ src: './MabryPro-LightItalic.woff2' })
+const mabryProBoldItalic = localFont({ src: './MabryPro-BoldItalic.woff2' })
+const mabryProLight = localFont({ src: './MabryPro-Light.woff2' })
+const mabryProBlackItalic = localFont({ src: './MabryPro-BlackItalic.woff2' })
+const mabryProMediumItalic = localFont({ src: './MabryPro-MediumItalic.woff2' })
+const mabryProItalic = localFont({ src: './MabryPro-Italic.woff2' })
+const mabryProRegular = localFont({ src: './MabryPro-regular.woff2' })
 
-    
-    
-
-export const mabryProBold = localFont({src: './MabryPro-bold.woff2'})
-export const mabryProMedium = localFont({src: './MabryPro-Medium.woff2'})
-export const mabryProBlack = localFont({src: './MabryPro-Black.woff2'})
-export const mabryProLightItalic = localFont({src: './MabryPro-LightItalic.woff2'})
-export const mabryProBoldItalic = localFont({src: './MabryPro-BoldItalic.woff2'})
-export const mabryProLight = localFont({src: './MabryPro-Light.woff2'})
-export const mabryProBlackItalic = localFont({src: './MabryPro-BlackItalic.woff2'})
-export const mabryProMediumItalic = localFont({src: './MabryPro-MediumItalic.woff2'})
-export const mabryProItalic = localFont({src: './MabryPro-Italic.woff2'})
-export const mabryProRegular = localFont({src: './MabryPro-regular.woff2'})
-
+export { mabryProBold, mabryProMedium, mabryProBlack, mabryProLightItalic, mabryProBoldItalic, mabryProLight, mabryProBlackItalic, mabryProMediumItalic, mabryProItalic, mabryProRegular }
---
+++ src/app/assets/fonts/MabryPro.tsx
@@ -0,0 +1,16 @@
+import localFont from 'next/font/local'
+
+// const mabryProBold = localFont({ src: './MabryPro-bold.woff2' })
+// const mabryProMedium = localFont({ src: './MabryPro-Medium.woff2' })
+const mabryProBlack = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Black.woff2' })
+// const mabryProLightItalic = localFont({ src: './MabryPro-LightItalic.woff2' })
+// const mabryProBoldItalic = localFont({ src: './MabryPro-BoldItalic.woff2' })
+// const mabryProLight = localFont({ src: './MabryPro-Light.woff2' })
+// const mabryProBlackItalic = localFont({ src: './MabryPro-BlackItalic.woff2' })
+// const mabryProMediumItalic = localFont({ src: './MabryPro-MediumItalic.woff2' })
+// const mabryProItalic = localFont({ src: './MabryPro-Italic.woff2' })
+// const mabryProRegular = localFont({ src: './MabryPro-regular.woff2' })
+
+export {  mabryProBlack }
+
+// export { mabryProBold, mabryProMedium, mabryProBlack, mabryProLightItalic, mabryProBoldItalic, mabryProLight, mabryProBlackItalic, mabryProMediumItalic, mabryProItalic, mabryProRegular }
