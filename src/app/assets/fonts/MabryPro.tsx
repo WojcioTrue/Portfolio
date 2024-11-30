c112e32b7e606ed701e6a19fc1adf513b4da0b46
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-11-30 15:06:02 +0100

    X

--- src/app/assets/fonts/MabryPro.tsx
+++ src/app/assets/fonts/MabryPro.tsx
@@ -1,16 +1,16 @@
 import localFont from 'next/font/local'
 
-const mabryProBold = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-bold.woff2' })
-const mabryProMedium = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Medium.woff2' })
+// const mabryProBold = localFont({ src: './MabryPro-bold.woff2' })
+// const mabryProMedium = localFont({ src: './MabryPro-Medium.woff2' })
 const mabryProBlack = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Black.woff2' })
-const mabryProLightItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-LightItalic.woff2' })
-const mabryProBoldItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-BoldItalic.woff2' })
-const mabryProLight = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Light.woff2' })
-const mabryProBlackItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-BlackItalic.woff2' })
-const mabryProMediumItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-MediumItalic.woff2' })
-const mabryProItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Italic.woff2' })
-const mabryProRegular = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-regular.woff2' })
+// const mabryProLightItalic = localFont({ src: './MabryPro-LightItalic.woff2' })
+// const mabryProBoldItalic = localFont({ src: './MabryPro-BoldItalic.woff2' })
+// const mabryProLight = localFont({ src: './MabryPro-Light.woff2' })
+// const mabryProBlackItalic = localFont({ src: './MabryPro-BlackItalic.woff2' })
+// const mabryProMediumItalic = localFont({ src: './MabryPro-MediumItalic.woff2' })
+// const mabryProItalic = localFont({ src: './MabryPro-Italic.woff2' })
+// const mabryProRegular = localFont({ src: './MabryPro-regular.woff2' })
 
+export {  mabryProBlack }
 
-
-export { mabryProBold, mabryProMedium, mabryProBlack, mabryProLightItalic, mabryProBoldItalic, mabryProLight, mabryProBlackItalic, mabryProMediumItalic, mabryProItalic, mabryProRegular }
+// export { mabryProBold, mabryProMedium, mabryProBlack, mabryProLightItalic, mabryProBoldItalic, mabryProLight, mabryProBlackItalic, mabryProMediumItalic, mabryProItalic, mabryProRegular }
