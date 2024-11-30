6427536b76f36da343c6dcb28c8b9942bb8b18a2
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-11-30 13:38:50 +0100

    Testing crap

--- public/fonts/mabry_pro/MabryPro.tsx
+++ public/fonts/mabry_pro/MabryPro.tsx
@@ -1,63 +1,63 @@
-// import localFont from 'next/font/local'
+import localFont from 'next/font/local'
 
-// const mabryPro = localFont({
-//         src: [
-//             {
-//                 path: './MabryPro-bold.woff2',
-//                 weight: 'bold',
-//                 style: 'normal',
-//             },
-//             {
-//                 path: './MabryPro-Medium.woff2',
-//                 weight: '500',
-//                 style: 'normal',
-//            },
-//             {
-//                 path: './MabryPro-Black.woff2',
-//                 weight: '900',
-//                 style: 'normal',  
-//             },
-//             {
-//                 path: './MabryPro-LightItalic.woff2',
-//                 weight: '300',
-//                 style: 'italic',  
-//             },
-//             {
-//                 path: './MabryPro-BoldItalic.woff2',
-//                 weight: 'bold',
-//                 style: 'italic',  
-//             },
-//             {
-//                 path: './MabryPro-Light.woff2',
-//                 weight: '300',
-//                 style: 'italic',  
-//             },
-//             {
-//                 path: './MabryPro-Light.woff2',
-//                 weight: '300',
-//                 style: 'normal',  
-//             },
-//             {
-//                 path: './MabryPro-BlackItalic.woff2',
-//                 weight: '900',
-//             },
-//            {
-//                path: './MabryPro-MediumItalic.woff2',
-//                weight: '500',
-//                 style: 'italic',  
-//            },
-//             {
-//                 path: './MabryPro-Italic.woff2',
-//                 weight: 'normal',
-//                 style: 'italic',  
-//             },
-//             {
-//                 path: './MabryPro-Regular.woff2',
-//                weight: 'normal',
-//                 style: 'normal',  
-//             },
-//         ]
-//     })
+const mabryPro = localFont({
+        src: [
+            {
+                path: './MabryPro-bold.woff2',
+                weight: 'bold',
+                style: 'normal',
+            },
+            {
+                path: './MabryPro-Medium.woff2',
+                weight: '500',
+                style: 'normal',
+           },
+            {
+                path: './MabryPro-Black.woff2',
+                weight: '900',
+                style: 'normal',  
+            },
+            {
+                path: './MabryPro-LightItalic.woff2',
+                weight: '300',
+                style: 'italic',  
+            },
+            {
+                path: './MabryPro-BoldItalic.woff2',
+                weight: 'bold',
+                style: 'italic',  
+            },
+            {
+                path: './MabryPro-Light.woff2',
+                weight: '300',
+                style: 'italic',  
+            },
+            {
+                path: './MabryPro-Light.woff2',
+                weight: '300',
+                style: 'normal',  
+            },
+            {
+                path: './MabryPro-BlackItalic.woff2',
+                weight: '900',
+            },
+           {
+               path: './MabryPro-MediumItalic.woff2',
+               weight: '500',
+                style: 'italic',  
+           },
+            {
+                path: './MabryPro-Italic.woff2',
+                weight: 'normal',
+                style: 'italic',  
+            },
+            {
+                path: '../../../public/fonts/mabry_pro/MabryPro-Regular.woff2',
+               weight: 'normal',
+                style: 'normal',  
+            },
+        ]
+    })
     
     
 
