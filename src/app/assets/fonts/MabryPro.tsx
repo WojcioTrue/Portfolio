66b4958728c94be2989778029de4e0da74421fc3
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-11-30 15:14:26 +0100

    XYZ

--- src/app/assets/fonts/MabryPro.tsx
+++ src/app/assets/fonts/MabryPro.tsx
@@ -1,11 +1,11 @@
 import localFont from 'next/font/local'
 
-const mabryProBold = localFont({ src: '../../../../public/fonts/mabry_pro/' })
+const mabryProBold = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Bold.woff2' })
 const mabryProMedium = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Medium.woff2' })
 const mabryProBlack = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Black.woff2' })
 const mabryProLightItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-LightItalic.woff2' })
 const mabryProBoldItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-BoldItalic.woff2' })
-const mabryProLight = localFont({ src: './MabryPro-Light.woff2' })
+const mabryProLight = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Light.woff2' })
 const mabryProBlackItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-BlackItalic.woff2' })
 const mabryProMediumItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-MediumItalic.woff2' })
 const mabryProItalic = localFont({ src: '../../../../public/fonts/mabry_pro/MabryPro-Italic.woff2' })
