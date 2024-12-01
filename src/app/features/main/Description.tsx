0818bd869f0e5ebd5235be668c26043575d10fbe
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-01 19:18:51 +0100

    Changed font to lighter

--- src/app/features/main/Description.tsx
+++ src/app/features/main/Description.tsx
@@ -1,3 +1,4 @@
+import { mabryProLight } from '@/app/assets/fonts/MabryPro';
 import { motion } from 'framer-motion';
 
 const Description = () => {
@@ -15,7 +16,7 @@ const Description = () => {
               duration: 0.3,
               delay: 0.25,
               type: "Tween"
-            }} className={`text-[16px] text-my-gray mb-4 sm:text-[24px] text-center ml:text-left `}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</motion.p>
+            }} className={`text-[16px] text-my-gray mb-4 sm:text-[24px] text-center ml:text-left ${mabryProLight.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</motion.p>
   )
 }
 
--- src/app/features/navbar/navBarList/NavBarListElement.tsx
+++ src/app/features/navbar/navBarList/NavBarListElement.tsx
@@ -5,6 +5,7 @@ import NavBarIndicatorField from './NavBarIndicatorField'
 import { NavBarListElementType } from '../navBarTypes'
 import { getIndicatorPosition } from '../navBarHooks/useDefaultIndicator'
 import { clickChangeCenter } from "../navBarHooks/useDefaultBackground"
+import { mabryProLight } from "@/app/assets/fonts/MabryPro"
 
 const NavBarListElement = ({ id, text, isActive }: NavBarListElementType) => {
   const { navBarDesktopPosition, navBarItems, indicatorDesktop } = useContext(NavBarContext)
