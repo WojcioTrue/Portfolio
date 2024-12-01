6eb4b0af0c80bfcf5d39489d61a902bdb310adb9
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-01 20:54:38 +0100

    Changing indicator position while element is clicked

--- src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
+++ src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { NavBarContext } from "../../navBarContext/NavBarContextProvider"
 import { useContext } from 'react'
 import { categories } from '../../navItems'
 import { NavBarListElementType } from '../../navBarTypes'
+import { getIndicatorPosition } from "../../navBarHooks/useDefaultIndicator"
 
 const MobileListElement = ({ text, id, isActive }: NavBarListElementType) => {
-  const { navBarItems } = useContext(NavBarContext)
+  const { navBarItems, indicatorDesktop } = useContext(NavBarContext)
+  const { setIndicatorPosition } = indicatorDesktop
 
   return (
     <li onClick={
       (event: React.MouseEvent) => {
         navBarItems.setListElements(categories.map(x => x.section === text ? { ...x, active: true } : x))
+        setIndicatorPosition(getIndicatorPosition(text))
       }
     } id={id} className={`mobile-nav-bar-element px-2 py-2 pl-3`}>
       <a className={`flex items-center justify-start ${isActive ? `text-white` : `text-my-black`}`}>
