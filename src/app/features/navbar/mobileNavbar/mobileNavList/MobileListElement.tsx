b5b3dc85e488104e6762e852c73e8f010124fbdc
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-02 20:16:30 +0100

    Imported NavBarIndicatorIcon to NavBarListElement

--- src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
+++ src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react'
 import { categories } from '../../navItems'
 import { NavBarListElementType } from '../../navBarTypes'
 import { getIndicatorPosition } from "../../navBarHooks/useDefaultIndicator"
+import NavBarIndicatorIcon from "../../navBarList/NavBarIndicatorIcon"
 
 const MobileListElement = ({text, id, isActive }: NavBarListElementType) => {
   const { navBarItems, indicatorDesktop } = useContext(NavBarContext)
@@ -14,22 +15,16 @@ const MobileListElement = ({text, id, isActive }: NavBarListElementType) => {
   }
 
   return (
-    <li onClick={
-      (event: React.MouseEvent) => {
+    <li onClick={() => {
         navBarItems.setListElements(categories.map(x => x.section === text ? { ...x, active: true } : { ...x, active: false }))
         changeIndicator(text)
       }
-    } id={id} className={`mobile-nav-bar-element px-2 py-2 pl-3`}>
+    } 
+    id={id} 
+    className={`mobile-nav-bar-element px-2 py-2 pl-3`}>
+      <NavBarIndicatorIcon/>
       <a className={`flex items-center justify-start ${isActive ? `text-white` : `text-my-black`}`}>
         {text}
-        {/* <Image
-          priority={true}
-          src={image}
-          alt="Logo"
-          width="20"
-          height="20"
-          className='ml-2'
-        /> */}
       </a>
     </li>
   )
