5cfd27215a1bb1d9e4c9011691cb56819dffef75
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-02 20:07:24 +0100

    Formatted MobileNavBarList

--- src/app/features/navbar/mobileNavbar/mobileNavList/MobileNavBarList.tsx
+++ src/app/features/navbar/mobileNavbar/mobileNavList/MobileNavBarList.tsx
@@ -9,18 +9,18 @@ const MobileUnList = () => {
 
   return (
     <div className='h-full w-full flex flex-col pl-3 mt-3'>
-      <hr/>
+      <hr />
       <label id="mobile-navbar-list" htmlFor="mobile-navbar-list" className={`w-full text-left pl-2 py-2 text-xl ${active ? 'text-my-black' : 'text-white'}`}>Menu Items</label>
       <ul className='w-full pl-2' id="mobile-navbar-list">
-        {navBarItems.listElements.map((listItem) => 
-        <MobileListElement 
-        key={`${listItem.section}`} 
-        id={`mobile_navbar_li_${listItem.section}`} 
-        text={listItem.section} 
-        isActive={listItem.active}
- />)}
+        {navBarItems.listElements.map((listItem) =>
+          <MobileListElement
+            key={`${listItem.section}`}
+            id={`mobile_navbar_li_${listItem.section}`}
+            text={listItem.section}
+            isActive={listItem.active}
+          />)}
       </ul>
-          <hr/>
+      <hr />
     </div>
 
   )
