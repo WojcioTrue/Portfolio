6c258ed8c23cfa24300b4a962f3e48ed734b5f09
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-02 22:23:52 +0100

    Added animation for indicator in navBar

--- src/app/features/navbar/mobileNavbar/mobileNavList/MobileIndicatorIcon.tsx
+++ src/app/features/navbar/mobileNavbar/mobileNavList/MobileIndicatorIcon.tsx
@@ -1,18 +1,29 @@
 import { motion } from 'framer-motion'
 
-const MobileIndicatorIcon = () => {
+const spring = {
+  type: "ease",
+  stiffness: 700,
+  damping: 30,
+  duration: 1
+};
+
+const MobileIndicatorIcon = ({ active }: { active: boolean }) => {
 
   return (
     <motion.div
-      className={`h-[30px] w-[30px] relative`}
+      className={`h-[30px] w-[30px] relative `}
       layout
-      transition={{
-        type: 'spring',
-        duration: 0.6
+      initial={{
+        opacity: 0,
+        translateX: '8px',
+      }}
+      animate={{ 
+        opacity: active ? 1 : 0,
+        translateX: active ? '0px' : '8px',
       }}
     >
       <motion.span
-        className='absolute w-[25px] h-[25px] rounded-2xl bg-my-purple top-[50%] left-[50%]'
+        className='absolute w-[20px] h-[20px] rounded-2xl bg-my-purple top-[50%] left-[50%]'
         initial={{
           translateY: '-50%',
           translateX: '-50%',
@@ -28,7 +39,7 @@ const MobileIndicatorIcon = () => {
           repeat: Infinity,
         }}
       ></motion.span>
-      <motion.span className='absolute w-[20px] h-[20px] rounded-2xl bg-my-purple top-[50%] left-[50%]'
+      <motion.span className='absolute w-[15px] h-[15px] rounded-2xl bg-my-purple top-[50%] left-[50%]'
         initial={{
           translateY: '-50%',
           translateX: '-50%',
@@ -43,7 +54,7 @@ const MobileIndicatorIcon = () => {
           times: [0, 0.5, 1],
           repeat: Infinity,
         }}></motion.span>
-      <motion.span className='absolute w-[10px] h-[10px] rounded-2xl bg-my-purple opacity-55 top-[50%] left-[50%]'
+      <motion.span className='absolute w-[7px] h-[7px] rounded-2xl bg-my-purple opacity-55 top-[50%] left-[50%]'
         initial={{
           translateY: '-50%',
           translateX: '-50%',
--- src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
+++ src/app/features/navbar/mobileNavbar/mobileNavList/MobileListElement.tsx
@@ -5,27 +5,39 @@ import { categories } from '../../navItems'
 import { NavBarListElementType } from '../../navBarTypes'
 import { getIndicatorPosition } from "../../navBarHooks/useDefaultIndicator"
 import MobileIndicatorIcon from "./MobileIndicatorIcon"
+import { motion } from "framer-motion"
 
-const MobileListElement = ({text, id, isActive }: NavBarListElementType) => {
+
+const MobileListElement = ({ text, id, isActive }: NavBarListElementType) => {
   const { navBarItems, indicatorDesktop } = useContext(NavBarContext)
   const { setIndicatorPosition } = indicatorDesktop
 
-  const changeIndicator = (arg : string) => {
+  const changeIndicator = (arg: string) => {
     setIndicatorPosition(getIndicatorPosition(arg))
   }
 
   return (
     <li onClick={() => {
-        navBarItems.setListElements(categories.map(x => x.section === text ? { ...x, active: true } : { ...x, active: false }))
-        changeIndicator(text)
-      }
-    } 
-    id={id} 
-    className={`mobile-nav-bar-element py-2 pl-1 flex`}>
-      <MobileIndicatorIcon/>
-      <a className={`flex items-center justify-start ${isActive ? `text-my-purple` : `text-my-black`}`}>
+      navBarItems.setListElements(categories.map(x => x.section === text ? { ...x, active: true } : { ...x, active: false }))
+      changeIndicator(text)
+    }
+    }
+      id={id}
+      className={`mobile-nav-bar-element py-2 pl-1 flex`}>
+      <MobileIndicatorIcon active={isActive} />
+      <motion.a
+        layout
+        initial={{
+          translateX: '-15px',
+        }}
+        animate={{
+          translateX: isActive ? '0px' : '-15px',
+        }}
+        className={`flex items-center justify-start 
+      ${isActive ? `text-my-purple` : `text-my-black`} 
+`}>
         {text}
-      </a>
+      </motion.a>
     </li>
   )
 }
