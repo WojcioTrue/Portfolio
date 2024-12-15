8302d5169e396b23c5662c64e4121aa996ce82c7
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-15 14:15:18 +0100

    Changed height for mobile navbar

--- src/app/features/navbar/NavBarToogleButton.tsx
+++ src/app/features/navbar/NavBarToogleButton.tsx
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react'
 import { NavBarContext } from "./navBarContext/NavBarContextProvider"
+import { motion } from 'framer-motion'
+import { HiOutlineXMark } from 'react-icons/hi2'
 
 const NavBarToogleButton = () => {
-     const {toogleMobileNav} = useContext(NavBarContext)
-     const {setDisplayMenu} = toogleMobileNav
+    const { toogleMobileNav } = useContext(NavBarContext)
+    const { displayMenu,setDisplayMenu } = toogleMobileNav
     return (
-        <button
-            className="float-right md:hidden px-2 border-red-400 border-2"
+        <motion.button
+            animate={{
+                rotate: displayMenu ? 90 : 0,
+            }}
+            transition={{
+                delay: 0.2
+            }}
+            className="h-12 w-12 md:hidden flex justify-center items-center"
             type="button"
             aria-controls="dropdown__navbar"
             aria-expanded="false"
             aria-label="Toggle navbar"
             onClick={() => setDisplayMenu(prev => !prev)}
-        >X</button>
+        >
+            <HiOutlineXMark size={'25px'} />
+        </motion.button>
     )
 }
 
