03251b02ee80d759e9b116bb29d332213d37ffce
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-15 14:20:59 +0100

    Created universal component for ButtonMenu

---
+++ src/app/shared_components/ToggleMenuButton.tsx
@@ -0,0 +1,31 @@
+import { HiOutlineXMark } from "react-icons/hi2";
+import { motion } from 'framer-motion'
+import { SetStateAction, Dispatch } from "react"
+
+type ToggleMenuButtonType = {
+    displayMenu: boolean,
+    setDisplayMenu: Dispatch<SetStateAction<boolean>>
+}
+
+const ToggleMenuButton = ({displayMenu, setDisplayMenu} : ToggleMenuButtonType) => {
+    return (
+        <motion.button
+            animate={{
+                rotate: displayMenu ? 90 : 0,
+            }}
+            transition={{
+                delay: 0.2
+            }}
+            className="h-12 w-12 md:hidden flex justify-center items-center"
+            type="button"
+            aria-controls="dropdown__navbar"
+            aria-expanded="false"
+            aria-label="Toggle navbar"
+            onClick={() => setDisplayMenu(prev => !prev)}
+        >
+            <HiOutlineXMark size={'25px'} />
+        </motion.button>
+    )
+}
+
+export default ToggleMenuButton
