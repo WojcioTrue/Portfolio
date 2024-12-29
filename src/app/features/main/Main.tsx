24137cdf8e00737c751c8eaffe06a73abc877ea7
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-12-29 13:45:28 +0100

    Added responsivity to Main Container

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -5,8 +5,8 @@ import MainIcons from './Icons/MainIcons';
 
 const Main = () => {
   return (
-    <div className="relative min-h-[100%] w-full flex flex-row justify-center px-8 bg-gradient-to-br from-my-lightBackground from-10% via-white">
-      <div className='w-max-[1200px]'>
+    <div className="relative min-h-[100%] w-full flex flex-row justify-center items-center px-8 bg-gradient-to-br from-my-lightBackground from-10% via-white">
+      <div className='w-max-[1200px] flex flex-col ml:flex-row'>
         <MainText />
         <MainIcons />
       </div>
