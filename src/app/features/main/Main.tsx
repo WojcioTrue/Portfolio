393b479bfceb7babc740222ac9a176a7effd8556
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-04 20:47:25 +0100

    Added temporary icon for html with white filling

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React from 'react'
 import MainText from './text/MainText';
+import IconTemplate from './text/Icons/IconTemplate';
 
 const Main = () => {
   return (
     <div className="relative flex min-h-[100%] px-8 bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center">
       <div className='w-max-[1200px] flex flex-col items-center gap-12 pt-24 ml:flex-row ml:gap-12 ml:pt-0 start'>
         <MainText />
+        <IconTemplate/>
       </div>
     </div>
   )
--- src/app/features/main/text/Icons/IconTemplate.tsx
+++ src/app/features/main/text/Icons/IconTemplate.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image"
+
 
 const IconTemplate = () => {
-  return (
-    <div>IconTemplate</div>
-  )
+    return (
+        <div className="bg-gradient-to-tr from-my-gradient1 to-my-gradient2 opacity-55 w-[40px] h-[40px] shadow-myshadow rounded-md p-[7px]">
+            <Image
+                alt="#"
+                width={50}
+                height={50}
+                src="/images/icon_html.png"
+            />
+        </div>
+    )
 }
 
 export default IconTemplate
