c2e83610f5754cfa994373b7ab465ae141b162f0
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-05 13:54:46 +0100

    Changed order in Icons Array\

--- src/app/features/main/text/backgroundIcons/IconsWrapper.tsx
+++ src/app/features/main/text/backgroundIcons/IconsWrapper.tsx
@@ -10,64 +10,65 @@ const IconsWrapper = () => {
     const arrOfIcons = [
         {
             id: 0,
-            name: 'css',
-            left: 3,
-            top: 3,
-        },
-        {
-            id: 1,
-            name: 'eslint',
-            left: 19,
-            top: 95,
-        },
-        {
-            id: 2,
             name: 'html',
             left: 21,
             top: 6,
         },
         {
-            id: 3,
+            id: 1,
+            name: 'css',
+            left: 3,
+            top: 3,
+        },
+        {
+            id: 2,
             name: 'js',
             left: 12,
             top: 25,
         },
         {
-            id: 4,
-            name: 'next',
-            left: 80,
-            top: 87,
-        },
-        {
-            id: 5,
-            name: 'prettier',
-            left: 8,
-            top: 75,
-        },
-        {
-            id: 6,
+            id: 3,
             name: 'react',
             left: 68,
             top: 72,
         },
         {
-            id: 7,
+            id: 4,
             name: 'redux',
             left: 90,
             top: 18,
         },
         {
+            id: 5,
+            name: 'ts',
+            left: 90,
+            top: 68,
+        },
+        {
+            id: 6,
+            name: 'next',
+            left: 80,
+            top: 87,
+        },
+        {
+            id: 7,
+            name: 'prettier',
+            left: 8,
+            top: 75,
+        },
+        {
             id: 8,
+            name: 'eslint',
+            left: 19,
+            top: 95,
+        },
+        {
+            id: 9,
             name: 'sass',
             left: 80,
             top: 5,
         },
-        {
-            id: 9,
-            name: 'ts',
-            left: 90,
-            top: 68,
-        }
+        
     ]
     return (
         <>
