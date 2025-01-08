27061b9372409856994b19801be8c5dac6d85ceb
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-08 21:03:49 +0100

    creating context to blur page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Banner from "./features/main/Main";
 import Tech from "./features/technologies/Tech";
 import NavBarContextWrapper from "./features/navbar/NavBarContextWrapper";
+import { createContext, useState } from "react";
+
+export const blurPage = createContext(false)
 
 export default function Home() {
+  const [blurPage, setBlurPage] = useState<boolean>(false)
   return (
     <>
       <NavBarContextWrapper />
-      <span className="blur-sm">
+      <span className={blurPage ? "blur-sm" : ""}>
         <Banner />
         <Tech />
       </span>
