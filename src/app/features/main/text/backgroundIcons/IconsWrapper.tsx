faf3bc1537ed5a24d984d3eb1ecf125c9c8d410b
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-08 20:26:56 +0100

    Removed sytax error/import bug from IconsWrapper

--- src/app/features/main/text/backgroundIcons/IconsWrapper.tsx
+++ src/app/features/main/text/backgroundIcons/IconsWrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import IconTemplate from './IconTemplate'
-import { arrOfIcons } from './IconsArray'
+import { arrOfIcons } from './iconsArray'
 
 const IconsWrapper = () => {
 
