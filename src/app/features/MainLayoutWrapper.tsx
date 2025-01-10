cc30faf9d5a287ff28e5570e717c7c468504df13
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-10 23:04:41 +0100

    Updated imports for BackDropContext

--- src/app/features/MainLayoutWrapper.tsx
+++ src/app/features/MainLayoutWrapper.tsx
@@ -1,14 +1,19 @@
 "use client"
+import { useContext } from "react";
 import Banner from "./main/Main";
 import Tech from "./technologies/Tech";
+import { BlurPage } from "./navbar/mobileNavbar/backdrop/BackDropContext";
 
 const MainLayoutWrapper = () => {
-  return (
-    <span className={/**${isBlur ? "blur-sm" : "" */`} scale-150 transition-all duration-500`}>
-    <Banner />
-    <Tech />
-  </span>
-  )
+    const { blurPage } = useContext(BlurPage)
+    const { isBlur } = blurPage
+
+    return (
+        <span className={`${isBlur ? "blur-sm" : "" } scale-150 transition-all duration-500`}>
+            <Banner />
+            <Tech />
+        </span>
+    )
 }
 
 export default MainLayoutWrapper
