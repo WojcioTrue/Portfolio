dda0c8ab90f78d3654f3038f03a7d055e7b32100
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-11 15:35:20 +0100

    Added padding and margin to Main component

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -3,10 +3,7 @@ import MainText from './text/MainText';
 
 const Main = () => {
   return (
-    <div className="relative flex min-h-[100%] px-8 landscape:pt-[50px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden" style={{
-      paddingTop: '100px',
-
-    }}>
+    <div className="relative flex min-h-[100%] px-8 landscape:pt-[75px] mb-[35px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
         <MainText />
     </div>
   )
