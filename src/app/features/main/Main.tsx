75ca47f63f5b576b9e17bcb30754da27c2ff5c44
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-11 15:41:31 +0100

    Testing responsive view

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -3,7 +3,7 @@ import MainText from './text/MainText';
 
 const Main = () => {
   return (
-    <div className="relative flex min-h-[100%] px-8 landscape:pt-[75px] mb-[35px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
+    <div className="relative flex min-h-[100%] px-8 landscape:pt-[75px] landscape:pb-[50px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
         <MainText />
     </div>
   )
