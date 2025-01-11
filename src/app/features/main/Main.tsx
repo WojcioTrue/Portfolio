f5ee056f47782dbc37d295b21a468bc9f25f0bbd
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-11 15:25:06 +0100

    Added padding when landscape view is On

--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from 'react'
 import MainText from './text/MainText';
 
 const Main = () => {
   return (
-    <div className="relative flex min-h-[100%] px-8 bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
+    <div className="relative flex min-h-[100%] px-8 landscape:pt-[50px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden" style={{
+      paddingTop: '100px',
+
+    }}>
         <MainText />
     </div>
   )
