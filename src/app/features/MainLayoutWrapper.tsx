ae376543d54fa9a99b2e848d95987dbec44286c6
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-16 22:18:15 +0100

    Added Default values for mobileNavBar ids

--- src/app/features/MainLayoutWrapper.tsx
+++ src/app/features/MainLayoutWrapper.tsx
@@ -3,34 +3,35 @@ import { useContext, useEffect } from "react";
 import Banner from "./main/Main";
 import Tech from "./technologies/Tech";
 import { BlurPage } from "./navbar/mobileNavbar/backdrop/BackDropContext";
+import NavBarContext from "./navbar/NavBarContextWrapper";
 
 const MainLayoutWrapper = () => {
     const { blurPage } = useContext(BlurPage)
     const { isBlur } = blurPage
 
     useEffect(() => {
-
         const checkId = () => {
-            const arrOfId = ['tech', 'tech2']
-            
+            const arrOfId = ['default', 'tech', 'tech2']
+
             arrOfId.forEach((x) => {
                 const idTestTop = document.getElementById(x)?.getBoundingClientRect().top
-                if(idTestTop !== undefined){
-                    console.log(((idTestTop >= -100) && (idTestTop < 100)) && console.log(true, x) )
+                if (idTestTop !== undefined) {
+                    console.log(((idTestTop >= -100) && (idTestTop < 100)) && console.log(true, x))
                 }
-          
             })
-          
-
         }
 
         document.addEventListener('scroll', () => {
-          checkId()
-        })
-      },[])
+            checkId()
+        }); return () => {
+            document.removeEventListener('scroll', () => {
+                checkId()
+            })
+        }
+    }, [])
 
     return (
-        <span className={`${isBlur ? "blur-sm" : "" } transition-all duration-500`}>
+        <span className={`${isBlur ? "blur-sm" : ""} transition-all duration-500`}>
             <Banner />
             <Tech />
         </span>
--- src/app/features/main/Main.tsx
+++ src/app/features/main/Main.tsx
@@ -3,7 +3,7 @@ import MainText from './text/MainText';
 
 const Main = () => {
   return (
-    <div className="relative flex min-h-[100%] px-8 landscape:pt-[80px] landscape:pb-[50px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
+    <div id="default" className="relative flex min-h-[100%] px-8 landscape:pt-[80px] landscape:pb-[50px] bg-gradient-to-br from-my-lightBackground from-10% via-white items-center justify-center overflow-hidden">
         <MainText />
     </div>
   )
