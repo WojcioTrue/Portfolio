d0ac1bd2b922007517eb8a1b84989e0bdc26730e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-01-18 16:07:39 +0100

    Created scroll function

--- src/app/features/MainLayoutWrapper.tsx
+++ src/app/features/MainLayoutWrapper.tsx
@@ -3,14 +3,42 @@ import { useContext, useEffect } from "react";
 import Banner from "./main/Main";
 import Tech from "./technologies/Tech";
 import { BlurPage } from "./navbar/mobileNavbar/backdrop/BackDropContext";
+import { NavBarContext } from "./navbar/navBarContext/NavBarContextProvider";
 
 const MainLayoutWrapper = () => {
+    const { navBarItems } = useContext(NavBarContext)
     const { blurPage } = useContext(BlurPage)
     const { isBlur } = blurPage
+    const { listElements, setListElements } = navBarItems
+
+    const sectionsArr = ['default', 'Welcome', 'About']
+
 
     useEffect(() => {
-        console.log('wrapper')
-    },[])
+        let activeElement = 'default'
+        const scrollEv = () => {
+            for (const section of sectionsArr) {
+                const sectionById = document.getElementById(section)!
+                const changeRange = window.scrollY - sectionById.offsetTop
+                if ((changeRange <= 40) && (changeRange >= -40)) {
+                    if (section !== activeElement) {
+                        activeElement = section
+                        setListElements(listElements.map(x => x.section === section ? { ...x, active: true } : { ...x, active: false }))
+                    }
+                }
+
+            }
+        }
+
+        window.addEventListener('scroll', () => {
+            scrollEv()
+        })
+        return () => {
+            window.removeEventListener('scroll', () => [
+                scrollEv()
+            ])
+        }
+    }, [])
 
     return (
         <span className={`${isBlur ? "blur-sm" : ""} transition-all duration-500`}>
