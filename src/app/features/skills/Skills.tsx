9aeac2b3e2aae767cab3cbe1ac79d39aafcacd7b
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-02 15:31:44 +0100

    Added temporary component

--- src/app/features/skills/Skills.tsx
+++ src/app/features/skills/Skills.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import TempSkills from './TempSkills'
 
 const Skills
  = () => {
   return (
     <>
-      <div id="Skills" className="relative h-full bg-cyan-400">Welcome</div>
+      <TempSkills/>
       <div id="About" className="relative h-full bg-yellow-400">About</div>
       <div id="Text" className="relative h-full bg-red-600">Text</div>
       <div id="Something" className="relative h-full bg-yellow-900">Something</div>
---
+++ src/app/features/skills/TempSkills.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TempSkills = () => {
+  return (
+    <div id="Skills" className="relative h-full bg-cyan-400">Welcome</div>
+  )
+}
+
+export default TempSkills
