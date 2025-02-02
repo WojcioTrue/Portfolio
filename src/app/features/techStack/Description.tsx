4f3a471d5c1b8656d4c4869326ebdfd28588ee3a
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-02 16:34:08 +0100

    Moved header and description to separate files

---
+++ src/app/features/techStack/Description.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { mabryProLight } from '@/app/assets/fonts/MabryPro'
+const Description = () => {
+  return (
+    <p className={`text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>My set of technologies used to build web apps.</p>
+  )
+}
+
+export default Description
---
+++ src/app/features/techStack/Header.tsx
@@ -0,0 +1,7 @@
+import { mabryProBold } from '@/app/assets/fonts/MabryPro'
+const Header = () => {
+    return (
+        <h1 className={`text-[42px] sm:text-[50px] md:text-[60px] xl:text-[68px] 2xl:text-[72px] text-center pt-10 ${mabryProBold.className}`}>Technology stack.</h1>)
+}
+
+export default Header
