54511a9a644e5286565c90daaf3e027e44e13760
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-03 22:19:52 +0100

    Styled description in header

--- src/app/features/main/text/Description.tsx
+++ src/app/features/main/text/Description.tsx
@@ -4,19 +4,20 @@ import { motion } from 'framer-motion';
 const Description = () => {
   return (
     <motion.p
-            initial={{
-              opacity: 0,
-              translateX: '-10px'
-            }}
-            animate={{
-              opacity: 1,
-              translateX: '0px'
-            }}
-            transition={{
-              duration: 0.3,
-              delay: 0.25,
-              type: "Tween"
-            }} className={`text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>Work in progress... be patient :)</motion.p>
+      initial={{
+        opacity: 0,
+        translateX: '-10px'
+      }}
+      animate={{
+        opacity: 1,
+        translateX: '0px'
+      }}
+      transition={{
+        duration: 0.3,
+        delay: 0.25,
+        type: "Tween"
+      }} className={`w-full text-header-desc-default
+    sm:text-header-desc-sm-default tracking-description text-my-gray pb-6 text-center ${mabryProLight.className}`}>Work in progress... be patient :)</motion.p>
   )
 }
 
