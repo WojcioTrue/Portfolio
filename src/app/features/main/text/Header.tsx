ef4be312b2f7c2f8dc91cf33700f5458bb4fcaa1
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-03 22:16:14 +0100

    Changed styles in header

--- src/app/features/main/text/Header.tsx
+++ src/app/features/main/text/Header.tsx
@@ -4,25 +4,25 @@ import { mabryProBold } from '@/app/assets/fonts/MabryPro';
 const Header = () => {
   return (
     <motion.h1
-            initial={{
-              opacity: 0,
-              translateX: '-10px'
-            }}
-            animate={{
-              opacity: 1,
-              translateX: '0px'
-            }}
-            transition={{
-              duration: 0.3,
-              delay: 0.2,
-              type: "Tween"
-            }}
-            className={` 
-      text-[42px] 
-      sm:text-[50px] 
-      md:text-[60px] 
-      xl:text-[68px] 
-      2xl:text-[72px]
+      initial={{
+        opacity: 0,
+        translateX: '-10px'
+      }}
+      animate={{
+        opacity: 1,
+        translateX: '0px'
+      }}
+      transition={{
+        duration: 0.3,
+        delay: 0.2,
+        type: "Tween"
+      }}
+      className={` 
+      text-header-my-default 
+      sm:text-header-my-sm 
+      md:text-header-my-md 
+      xl:text-header-my-xl 
+      2xl:text-header-my-2xl 
       text-center
       ${mabryProBold.className}`}>Wojcio_True</motion.h1>
   )
