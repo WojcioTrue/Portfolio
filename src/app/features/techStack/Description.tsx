a4310be63bc2f092751aa8f232976782f3535445
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-03 20:51:05 +0100

    Added animation to description for tech stack

--- src/app/features/techStack/Description.tsx
+++ src/app/features/techStack/Description.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import { mabryProLight } from '@/app/assets/fonts/MabryPro'
+import { motion } from 'framer-motion'
 const Description = () => {
+
+  const description = ['My', ' ', 'set', ' ', 'of', ' ', 'technologies', ' ', 'used', ' ', 'to', ' ', 'build', ' ', 'web', ' ', 'apps', '.']
+
   return (
-    <p className={`text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>My set of technologies used to build web apps.</p>
+    <p className={`text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>
+      {description.map((x, i) =>
+        <motion.span
+          key={i}
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            delay: 1 + i/20
+          }}
+          >{x}</motion.span>
+      )}
+    </p>
   )
 }
 
