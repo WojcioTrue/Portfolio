dbcfc518e38fbc29974938ab68a33f790c105ac8
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-03 21:43:36 +0100

    Added smooth animations for Header and description

--- src/app/features/techStack/Description.tsx
+++ src/app/features/techStack/Description.tsx
@@ -3,23 +3,26 @@ import { mabryProLight } from '@/app/assets/fonts/MabryPro'
 import { motion } from 'framer-motion'
 const Description = () => {
 
-  const description = ['My', ' ', 'set', ' ', 'of', ' ', 'technologies', ' ', 'used', ' ', 'to', ' ', 'build', ' ', 'web', ' ', 'apps', '.']
+  const description = ['My', 'set', 'of', 'technologies','used', 'to','build','web','apps', '.']
 
   return (
-    <p className={`text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>
+    <p className={`flex justify-center gap-[6px] text-[16px] w-full text-my-gray pb-6 sm:text-[24px] text-center ${mabryProLight.className}`}>
       {description.map((x, i) =>
         <motion.span
-        className=''
+        className='inline-block'
           key={i}
           initial={{
             opacity: 0,
-            y: '100px'
+            y: '10px',
+            x: '-2px'
           }}
           whileInView={{
             opacity: 1,
+            y: '0px',
+            x: '0px'
           }}
           transition={{
-            delay: 1 + i/30
+            delay: 1 + i/20
           }}
           viewport={{
             once: true,
--- src/app/features/techStack/Header.tsx
+++ src/app/features/techStack/Header.tsx
@@ -6,67 +6,69 @@ const Header = () => {
     const ref = useRef(null)
 
     return (
-        <div ref={ref}>
-            <h1 className={`text-[42px] sm:text-[50px] md:text-[60px] xl:text-[68px] 2xl:text-[72px] text-center pt-10 ${mabryProBold.className}`}>
-                <motion.span
-                    initial={{
-                        opacity: 0,
-                        y: 20
-                    }}
-                    whileInView={{
-                        opacity: 1,
-                        y: 0
-                    }}
-                    transition={{
-                        delay: 0.4,
-                        duration: 0.4
-                    }}
-                    viewport={{
-                        once: true,
-                        margin: '-100px'
-                    }}
-                >
-                    {"Technology "}
-                </motion.span>
-                <motion.span initial={{
+        <div ref={ref} className={`flex justify-center text-[42px] sm:text-[50px] md:text-[60px] xl:text-[68px] 2xl:text-[72px] text-center pt-10 tracking-[-5px] ${mabryProBold.className}`}>
+            <motion.h1
+                className='mr-5'
+                initial={{
                     opacity: 0,
                     y: 20
                 }}
-                    whileInView={{
-                        opacity: 1,
-                        y: 0
-                    }}
-                    transition={{
-                        delay: 0.6,
-                        duration: 0.4
-                    }}
-                    viewport={{
-                        once: true,
-                        margin: '-100px'
-                    }}
-                >
-                    {"stack"}
-                </motion.span>
-                <motion.span initial={{
+                whileInView={{
+                    opacity: 1,
+                    y: 0
+                }}
+                transition={{
+                    delay: 0.4,
+                    duration: 0.4
+                }}
+                viewport={{
+                    once: true,
+                    margin: '-100px'
+                }}
+            >
+                {"Technology "}
+            </motion.h1>
+            <motion.h1
+                className='mr-1'
+                initial={{
                     opacity: 0,
                     y: 20
                 }}
-                    whileInView={{
-                        opacity: 1,
-                        y: 0
-                    }}
-                    transition={{
-                        delay: 0.8,
-                        duration: 2
-                    }}
-                    viewport={{
-                        once: true,
-                        margin: '-100px'
-                    }}>
-                    {"."}
-                </motion.span></h1>
+                whileInView={{
+                    opacity: 1,
+                    y: 0
+                }}
+                transition={{
+                    delay: 0.6,
+                    duration: 0.4
+                }}
+                viewport={{
+                    once: true,
+                    margin: '-100px'
+                }}
+            >
+                {"stack"}
+            </motion.h1>
+            <motion.h1
+                initial={{
+                    opacity: 0,
+                    y: 20
+                }}
+                whileInView={{
+                    opacity: 1,
+                    y: 0
+                }}
+                transition={{
+                    delay: 0.8,
+                    duration: 2
+                }}
+                viewport={{
+                    once: true,
+                    margin: '-100px'
+                }}>
+                {"."}
+            </motion.h1>
         </div>
-
     )
 }
 
