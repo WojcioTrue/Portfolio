9e418021eff208f38b8a68eb2de09b67e5c91e77
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-05 00:09:46 +0100

    Added detection when element is dragged

--- src/app/features/techStack/gridOfSkills/SkillsGrid.tsx
+++ src/app/features/techStack/gridOfSkills/SkillsGrid.tsx
@@ -1,27 +1,28 @@
 import { motion } from 'framer-motion'
 import React, { useEffect, useRef, useState } from 'react'
-
+import detectElementOverflow from 'detect-element-overflow'
 
 
 const SkillsGrid = () => {
   const constraintDrag = useRef<HTMLDivElement>(null)
   const [isIn, setIsIn] = useState(false)
 
-  const dragElementFc = () => {
-    console.log('drag occuring')
-  }
-
   useEffect(() => {
-    const target = document.getElementById('drag-element')
-    target?.addEventListener('drag', () => {
-      console.log('triggered')
-    })
+
   })
 
   return (
     <div ref={constraintDrag} className='relative m-0 h-[300px] w-[300px] bg-red-500'>
       <motion.div
-        onDrag={() => console.log('x')}
+        id="drop-element"
+        onDrag={() => {
+          const element = document.getElementById('drop-element')!
+          const target = document.getElementById('drop-target')!
+          const collisions = detectElementOverflow(element, target);
+          collisions.overflowLeft >= 15 ?
+            setIsIn(false) : setIsIn(true)
+
+        }}
         className="bg-slate-600 w-[30px] h-[30px] absolute top-[70px] left-[20px] z-10"
         drag
         dragConstraints={constraintDrag}
@@ -34,7 +35,7 @@ const SkillsGrid = () => {
       </motion.div>
       <p>{isIn.toString()}</p>
       <div id="drop-target"
-        onDragOver={(e) => { e.preventDefault(), console.log('working') }}
+
         className='absolute right-[10px] top-[75px] h-[100px] w-[100px] bg-white'>
 
       </div>
