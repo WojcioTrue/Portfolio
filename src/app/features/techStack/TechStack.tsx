8215954b912ac52f345a96f7b5db341e55d69f34
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-02-04 21:13:42 +0100

    Added field for test run with moving element inside

--- src/app/features/techStack/TechStack.tsx
+++ src/app/features/techStack/TechStack.tsx
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Description from './Description'
+import SkillsGrid from './gridOfSkills/SkillsGrid'
 
 const TechStack = () => {
     return (
-        <div id="Skills" className="relative h-full bg-white border-t-[1px] border-my-lightGray" >
+        <div id="Skills" className="relative h-full bg-white border-t-[1px] border-my-lightGray overflow-hidden flex items-center flex-col " >
             <Header/>
             <Description/>
+            <SkillsGrid/>
         </div>
     )
 }
--- src/app/features/techStack/gridOfSkills/SkillsGrid.tsx
+++ src/app/features/techStack/gridOfSkills/SkillsGrid.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useRef, useState } from 'react'
+
 
 const SkillsGrid = () => {
+  const constraintDrag = useRef<HTMLDivElement>(null)
+  const [isIn, setIsIn] = useState(false)
   return (
-    <div>SkillsGrid</div>
+    <motion.div ref={constraintDrag} className='relative m-0 h-[300px] w-[300px] bg-red-500'>
+      <motion.div
+        className="bg-gray-600 w-[30px] h-[30px] absolute top-[70px] left-[20px] z-10"
+        drag
+        dragConstraints={constraintDrag}
+        dragElastic={0.03}
+        whileDrag={{
+          scale: 1.2,
+        }}
+      >
+      </motion.div>
+        <p>{isIn.toString()}</p>
+      <div className='absolute right-[10px] top-[75px] h-[100px] w-[100px] bg-white'>
+
+      </div>
+    </motion.div>
+
   )
 }
 
