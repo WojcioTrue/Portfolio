37e8676a5e6dd0befd5747c52daa0f1094ac6d6a
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-03-11 22:11:45 +0100

    Moved Skill Button to separate component

---
+++ src/app/features/skillPrompt/SkillButton.tsx
@@ -0,0 +1,54 @@
+import { motion } from 'framer-motion'
+import { HiOutlineXMark } from 'react-icons/hi2'
+import { SkillPromptContext } from './SkillPromptContextProvider'
+import { useContext } from 'react'
+
+const SkillButton = () => {
+    const { promptDisplay } = useContext(SkillPromptContext)
+    const {  setDisplay } = promptDisplay
+    return (
+        <motion.button
+            initial={{
+                opacity: 0,
+                scale: 1.6,
+            }}
+            animate={{
+                opacity: 1,
+                scale: 1,
+                transition: {
+                    opacity: {
+                        duration: 0.2,
+                        delay: 0.3
+                    },
+                    scale: {
+                        duration: 0.2,
+                        delay: 0.3
+                    }
+                }
+            }}
+            exit={{
+                scale: 1.6
+            }}
+            onClick={() => setDisplay(false)}
+            className='
+                    absolute
+                    flex
+                    items-center
+                    justify-center
+                    bg-white 
+                    right-[17px]
+                    top-[23px]
+                    rounded-3xl
+                    w-[35px] 
+                    h-[35px]'
+            type="button"
+            aria-controls="skill_prompt_close"
+            aria-expanded="false"
+            aria-label="skill_prompt_close"
+        >
+            <HiOutlineXMark size={'25px'} color="rgb(116, 31, 245)" />
+        </motion.button>
+    )
+}
+
+export default SkillButton
--- src/app/features/skillPrompt/SkillPrompt.tsx
+++ src/app/features/skillPrompt/SkillPrompt.tsx
@@ -1,12 +1,11 @@
 import React, { useContext, useEffect } from 'react'
 import { SkillPromptContext } from './SkillPromptContextProvider'
 import { AnimatePresence, motion } from 'framer-motion'
-import { HiOutlineXMark } from 'react-icons/hi2'
+import SkillButton from './SkillButton'
 
 const SkillPrompt = () => {
-    const { promptPosition, promptDisplay } = useContext(SkillPromptContext)
-    const { display, setDisplay } = promptDisplay
-    const { position } = promptPosition
+    const { promptDisplay } = useContext(SkillPromptContext)
+    const { display } = promptDisplay
 
     useEffect(() => {
         display ? document.body.classList.add('overflow-y-hidden') : document.body.classList.remove('overflow-y-hidden')
@@ -38,45 +37,7 @@ const SkillPrompt = () => {
                     }}
                     className={`fixed z-50 w-full h-full bg-gray-900 bg-opacity-85  overflow-hidden`}
                 >
-
-                    <motion.button
-                        initial={{
-                            opacity: 0,
-                            scale: 1.6,
-                        }}
-                        animate={{
-                            opacity: 1,
-                            scale: 1,
-                            transition: {
-                                opacity: {
-                                    duration: 0.2,
-                                    delay: 0.3
-                                },
-                                scale: {
-                                    duration: 0.2,
-                                    delay: 0.3
-                                }
-                            }
-                        }}
-                        onClick={() => setDisplay(false)}
-                        className='
-                                        absolute
-                                        flex
-                                        items-center
-                                        justify-center
-                                        bg-white 
-                                        right-[17px]
-                                        top-[23px]
-                                        rounded-3xl
-                                        w-[35px] 
-                                        h-[35px]'
-                        type="button"
-                        aria-controls="skill_prompt_close"
-                        aria-expanded="false"
-                        aria-label="skill_prompt_close"
-                    >
-                        <HiOutlineXMark size={'25px'} color="rgb(116, 31, 245)" />
-                    </motion.button>
+                    <SkillButton />
                 </motion.div>
             }
 
