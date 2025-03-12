69d41edebb93cb85feb79b327c581ab877b038cd
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-03-12 21:28:43 +0100

    Added initial position for prompt

--- src/app/features/skillPrompt/SkillInfo.tsx
+++ src/app/features/skillPrompt/SkillInfo.tsx
@@ -1,18 +1,32 @@
-
+import { useContext, useEffect } from "react"
+import { SkillPromptContext } from "./SkillPromptContextProvider"
+import { motion } from "framer-motion"
 
 const SkillInfo = () => {
+  const { promptPosition } = useContext(SkillPromptContext)
+  const { top, left } = promptPosition.position
+
+  useEffect(() => {
+    console.log(promptPosition)
+  },[])
+
   return (
-    <div className="
+    <motion.div className="
     absolute 
     top-1/2 
     left-1/2 
-    -translate-x-1/2
-    -translate-y-1/2
     bg-white 
-    w-[80%] 
-    h-[80%]
-    
-    ">SkillWrapper</div>
+    w-[250px] 
+    h-[120px]
+    rounded-[7px]
+    "
+    initial={{
+      top: top,
+      left: left,
+    }}
+    >
+      SkillWrapper
+      </motion.div>
   )
 }
 
