26a0304b34d861c9a46b642a352a1f9eb5873aab
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-04-18 17:47:20 +0200

    Added interval function to read initial props of InitialPrompt

--- src/app/features/skillPrompt/InitialPrompt.tsx
+++ src/app/features/skillPrompt/InitialPrompt.tsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState, useRef, useLayoutEffect } from 'react'
 import { SkillPromptContext } from './SkillPromptContextProvider'
 import Image from "next/image"
-//This component will be used to get initial height and position for prompt, also use values like height and position when window will be resized. For now (18.04.2025) i can't animate height from stiff value to auto, so i will create this dummy component to get all values and push to final Prompt compoent
+//This component will be used to get initial height and position for prompt, also use values like height and position when window will be resized. For now (18.04.2025) i can't animate height from stiff value (in pixels) to auto, so i will create this dummy component to get all values and push to final Prompt component
 
 const InitialPrompt = () => {
     const { promptPosition } = useContext(SkillPromptContext)
     const { promptSkill } = useContext(SkillPromptContext)
     const { skill } = promptSkill
+    const initialPromptRef = useRef<HTMLDivElement>(null)
+    const [elementProps, setElementProps] = useState<Object | null>(null)
     const [skillProperties, setSkillProperties] = useState({
         coords: {
             x: 0,
@@ -18,8 +20,26 @@ const InitialPrompt = () => {
         }
     })
 
+    useEffect(() => {
+        const updateWidth = setInterval(() => {
+            const elementProps = initialPromptRef.current?.getBoundingClientRect()!
+            console.log(elementProps.width)
+            if (elementProps.width !== 0) {
+                clearInterval(updateWidth)
+            }
+        }, 10)
+        return () => {
+            clearInterval(updateWidth)
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(elementProps)
+    }, [elementProps])
+
+
     return (
-        <div className="
+        <div id="initialPrompt" ref={initialPromptRef} className="
         relative
         rounded-[7px]
         overflow-hidden
@@ -28,8 +48,9 @@ const InitialPrompt = () => {
         w-[90%] sx:w-[350px]"
         >
             <div className="relative bg-red-500 w-[100%] h-[150px]">
-                <div className="bg-blue-500 absolute">
+                <div className="bg-blue-500 absolute left-[50%] top-[25%] h-[50px] w-[50px]">
                     <Image
+                        className=''
                         src={skill.src}
                         alt="#"
                         fill
@@ -41,6 +62,7 @@ const InitialPrompt = () => {
                 className="py-[30px] text-white mx-6">
                 <p className="text-4xl">{skill.name}</p>
                 <div className="w-full bg-my-lightBackground my-3 h-[1px]"></div>
+                <p>{skill.text}</p>
                 <p>{skill.text}</p>
             </div>
         </div>
