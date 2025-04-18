d60f5e964bb232c3e5e2ac7144603772748ecefc
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-04-18 15:45:33 +0200

    Styled Skill Image icon

--- src/app/features/skillPrompt/ImgSkill.tsx
+++ src/app/features/skillPrompt/ImgSkill.tsx
@@ -9,33 +9,38 @@ const ImgSkill = () => {
     // const { display } = promptDisplay.display
 
     return (
-        <motion.div
-            className="relative bg-red-500 w-full h-[200px]"
-            // initial={{
-            //     top: 20,
-            //     left: '50%',
-            //     translateX: "-50%",
-            //     width: 50,
-            //     height: 50
-            // }}
-            // animate={{
-            //     top: 70,
-            //     width: 100,
-            //     height: 100,
-            //     transition: {
-            //         duration: 0.2,
-            //         delay: 0.7
-            //     }
-            // }}
-        >
-            {/* <Image
-                src={skill.src}
-                alt="#"
-                fill
-                sizes="100%"
-            /> */}
+        <div
+            className="relative bg-red-500 w-[100%] h-[150px]"
 
-        </motion.div>
+        >
+            <motion.div
+                className="bg-blue-500 absolute"
+                initial={{
+                    top: '0%',
+                    left: '50%',
+                    translateX: "-50%",
+                    width: 50,
+                    height: 50
+                }}
+                animate={{
+                    top: '50%',
+                    translateY: '-50%',
+                    width: 100,
+                    height: 100,
+                    transition: {
+                        duration: 0.2,
+                        delay: 0.7
+                    }
+                }}>
+                <Image
+                    src={skill.src}
+                    alt="#"
+                    fill
+                />
+            </motion.div>
+
+
+        </div >
     )
 }
 
