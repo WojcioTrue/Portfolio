01b6c9e3b36d15a080cdfd358fddc178f8b937df
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-05-12 17:25:15 +0200

    Justified items to center of container

--- src/app/features/techStack/TechStack.tsx
+++ src/app/features/techStack/TechStack.tsx
@@ -6,7 +6,7 @@ import TechStackContextProvider from './techStackContext/TechStackContext'
 const TechStack = () => {
     return (
         <TechStackContextProvider>
-            <div id="Skills" className="relative min-h-[calc(100%_-_77px)] bg-white border-t-[1px] py-10 border-my-lightGray overflow-hidden flex items-center flex-col" >
+            <div id="Skills" className="relative min-h-[calc(100%_-_77px)] bg-white border-t-[1px] pt-10 pb-16 border-my-lightGray overflow-hidden flex items-center flex-col justify-center" >
                 <Header />
                 <Description />
                 <SkillsGrid />
