da9c1c2c3843d6946a499801266f4779a6dd8978
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-05-17 17:21:00 +0200

    Added refrence to update proper values while resizing

--- src/app/features/techStack/gridOfSkills/dropField/Icon.tsx
+++ src/app/features/techStack/gridOfSkills/dropField/Icon.tsx
@@ -25,7 +25,7 @@ const Icon = ({ dragElement, imgSrc, whiteImgSrc, text }: IconType) => {
     const animationControls = useAnimationControls()
     const dragElementRef = useRef<HTMLDivElement>(null)
     const [elementPos, setElementPos] = useState({ top: 0, left: 0 })
-    const [dropValues, setDropValues] = useState({ top: 0, left: 0 })
+    const dropValues = useRef({ top: 0, left: 0 })
     const [active, setActive] = useState<string>('')
 
 
@@ -47,15 +47,32 @@ const Icon = ({ dragElement, imgSrc, whiteImgSrc, text }: IconType) => {
 
 
             const tempLeft = dropTarget.current.offsetLeft + dropTargetWidth / 2 - element.clientWidth / 2 - element.offsetLeft
-            setDropValues({ top: tempTop, left: tempLeft })
+            dropValues.current = { top: tempTop, left: tempLeft }
 
         }
 
     }, [dragElement, dropTarget, elementPos])
 
-
     useEffect(() => {
-        const tempResize = () => {
+        window.addEventListener('resize', () => {
+            const element = dragElementRef.current!
+            if (dropTarget?.current !== undefined && dropTarget?.current !== null) {
+                const dropTargetHeight = dropTarget.current.clientHeight
+                const dropTargetWidth = dropTarget.current.clientWidth
+                const elementHeight = Math.ceil(element.clientHeight / 2)
+                const tempTop = dropTarget.current.offsetTop - element.offsetTop + dropTargetHeight / 2 - elementHeight
+
+                const tempLeft = dropTarget.current.offsetLeft + dropTargetWidth / 2 - element.clientWidth / 2 - element.offsetLeft
+                dropValues.current = { top: tempTop, left: tempLeft }
+            }
+        })
+    }, [dropTarget])
+
+
+
+    // update icon position after resizing
+    useEffect(() => {
+        const resizeIcons = () => {
             if (inTarget.id !== dragElement) {
                 animationControls.start({
                     x: 0,
@@ -63,9 +80,9 @@ const Icon = ({ dragElement, imgSrc, whiteImgSrc, text }: IconType) => {
                 })
             }
         }
-        window.addEventListener('resize', tempResize)
+        window.addEventListener('resize', resizeIcons)
         return () => {
-            window.removeEventListener('resize', tempResize)
+            window.removeEventListener('resize', resizeIcons)
         }
     }, [animationControls, dragElement, inTarget.id])
 
@@ -82,8 +99,8 @@ const Icon = ({ dragElement, imgSrc, whiteImgSrc, text }: IconType) => {
         if (overTarget.boolean) {
             droppedInField(el)
             animationControls.start({
-                x: dropValues.left,
-                y: dropValues.top,
+                x: dropValues.current.left,
+                y: dropValues.current.top,
             })
         }
         else {
@@ -93,6 +110,22 @@ const Icon = ({ dragElement, imgSrc, whiteImgSrc, text }: IconType) => {
             })
         }
     }
+
+    useEffect(() => {
+        const resize = () => {
+            if (inTarget.id === dragElement) {
+                animationControls.start({
+                    x: dropValues.current.left,
+                    y: dropValues.current.top,
+                })
+            }
+        }
+        window.addEventListener('resize', resize)
+
+        return () => {
+            window.removeEventListener('resize', resize)
+        }
+    }, [animationControls, dragElement, , inTarget.id])
 
     const draggedElement = () => {
         setActive(dragElement)
