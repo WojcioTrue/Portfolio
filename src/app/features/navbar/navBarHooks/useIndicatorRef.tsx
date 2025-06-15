a638204a15641e16eb04673c7402331ee482f20e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-15 20:08:01 +0200

    Removed dependency warning

--- src/app/features/navbar/navBarHooks/useIndicatorRef.tsx
+++ src/app/features/navbar/navBarHooks/useIndicatorRef.tsx
@@ -5,26 +5,25 @@ const useIndicatorRef = () => {
     const { defaultIndicatorRef, skillsIndicatorRef, aboutIndicatorRef, textIndicatorRef, somethingIndicatorRef, navBarItems } = useContext(NavBarContext)
     const { listElements } = navBarItems
 
-    const getRef = (arg: string) => {
-        if (arg === 'Skills') {
-            return skillsIndicatorRef
-        } else if (arg === 'About') {
-            return aboutIndicatorRef
-        } else if (arg === 'Text') {
-            return textIndicatorRef
-        } else if (arg === 'Something') {
-            return somethingIndicatorRef
-        }
-    }
-
     const getReference = useCallback(() => {
+        const getRef = (arg: string) => {
+            if (arg === 'Skills') {
+                return skillsIndicatorRef
+            } else if (arg === 'About') {
+                return aboutIndicatorRef
+            } else if (arg === 'Text') {
+                return textIndicatorRef
+            } else if (arg === 'Something') {
+                return somethingIndicatorRef
+            }
+        }
         const getActiveSection = listElements.filter(x => x.active === true)
         if (getActiveSection.length > 0) {
             return getRef(getActiveSection[0].section)
         } else {
             return defaultIndicatorRef
         }
-    }, [listElements])
+    }, [aboutIndicatorRef, defaultIndicatorRef, listElements, skillsIndicatorRef, somethingIndicatorRef, textIndicatorRef])
 
 
     return getReference()
