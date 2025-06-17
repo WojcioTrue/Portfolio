a2a80948e48f13463de13330fbf7751f49720324
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-17 23:03:20 +0200

    imported ref for default Li element

--- src/app/features/navbar/navBarHooks/useLiRef.tsx
+++ src/app/features/navbar/navBarHooks/useLiRef.tsx
@@ -1,5 +1,9 @@
+import { useCallback, useContext } from "react"
+import { NavBarContext } from "../navBarContext/NavBarContextProvider"
+
 
 const useLiRef = () => {
+    const {defaultLiRef} = useContext(NavBarContext)
   return (
     <div>useLiRef</div>
   )
