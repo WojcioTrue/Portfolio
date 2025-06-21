58d068e58e72a0033001b59cf8cf25ea3e5a04df
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-21 11:44:50 +0200

    Added function to return useLiRef for navBar list element

--- src/app/features/navbar/navBarHooks/useLiRef.tsx
+++ src/app/features/navbar/navBarHooks/useLiRef.tsx
@@ -1,12 +1,34 @@
-import { useContext } from "react"
+import { useContext, useMemo } from "react"
 import { NavBarContext } from "../navBarContext/NavBarContextProvider"
 
 
 const useLiRef = () => {
-    const { defaultLiRef } = useContext(NavBarContext)
-    return (
-        <div>useLiRef</div>
-    )
+    const { defaultLiRef, skillsLiRef, aboutLiRef, textLiRef, somethingLiRef, navBarItems } = useContext(NavBarContext)
+    const { listElements } = navBarItems
+
+    const getLiRef = useMemo(() => {
+        const getRef = (arg: string) => {
+            if (arg === 'Skills') {
+                return skillsLiRef
+            } else if (arg === 'About') {
+                return aboutLiRef
+            } else if (arg === 'Text') {
+                return textLiRef
+            } else if (arg === 'Something') {
+                return somethingLiRef
+            }
+        }
+        const getActiveSection = listElements.filter(x => x.active === true)
+        if (getActiveSection.length > 0) {
+            return getRef(getActiveSection[0].section)
+        } else {
+            return defaultLiRef
+        }
+    }, [aboutLiRef, defaultLiRef, listElements, skillsLiRef, somethingLiRef, textLiRef])
+
+
+
+    return getLiRef
 }
 
 export default useLiRef
