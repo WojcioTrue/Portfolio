f83f3f8029e42287b54cb05a4bc7f6c75f157130
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-22 14:43:23 +0200

    Imported references for mobileLielements

--- src/app/features/navbar/mobileNavbar/mobieNavBarHooks/useMobileLiRef.tsx
+++ src/app/features/navbar/mobileNavbar/mobieNavBarHooks/useMobileLiRef.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavBarContext } from '../../navBarContext/NavBarContextProvider'
 
 const useMobileLiRef = () => {
+  const { mobileSkillsLi, mobileAboutLi, mobileTextLi, mobileSomethingLi } = useContext(NavBarContext)
   return (
     <div>useMobileLiRef</div>
   )
