3c57f0c6d37055c74d5bc21814adf77484b8da3e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-22 15:18:45 +0200

    Added type to custom hook

--- src/app/features/navbar/mobileNavbar/animatedBg/MobileAnimatedBg.tsx
+++ src/app/features/navbar/mobileNavbar/animatedBg/MobileAnimatedBg.tsx
@@ -1,10 +1,14 @@
 import Bottom from './Bottom'
 import Top from './Top'
 import useDefaultBackground from '../mobieNavBarHooks/useDefaultBackground'
+import { useContext } from 'react'
+import { NavBarContext } from '../../navBarContext/NavBarContextProvider'
 
 const MobileAnimatedBg = () => {
-
-  useDefaultBackground()
+  const { navBarItems } = useContext(NavBarContext)
+  const activeLi = navBarItems.listElements.filter(x => x.active === true) 
+  console.log(activeLi)
+  useDefaultBackground(activeLi)
 
   return (
     <div className={`absolute top-0 h-[100%] w-full z-[-10]`}>
--- src/app/features/navbar/mobileNavbar/mobieNavBarHooks/useDefaultBackground.tsx
+++ src/app/features/navbar/mobileNavbar/mobieNavBarHooks/useDefaultBackground.tsx
@@ -1,33 +1,36 @@
 import { useContext, useEffect } from 'react'
 import { NavBarContext } from "./../../navBarContext/NavBarContextProvider"
+import { ListElement } from '../../navBarTypes'
 
-const useDefaultBackground = () => {
-   const { toogleMobileNav, navBarMobilePosition, mobileNavLabelRef } = useContext(NavBarContext)
-    const { displayMenu } = toogleMobileNav
-    const { navBarItems } = useContext(NavBarContext)
-    const { position, setPosition } = navBarMobilePosition
-  
-    useEffect(() => {
-      const labelElement = mobileNavLabelRef?.current!
-      const getActiveSection = navBarItems.listElements.filter(x => x.active === true)
-      if (displayMenu) {
-        if (getActiveSection.length > 0) {
-          const activeSectionName = `mobile_navbar_li_${getActiveSection[0].section}`
-          const firstLiElement = document.getElementById(activeSectionName)!;
-  
-          const topValue = Number(firstLiElement.getBoundingClientRect().top.toFixed(0))
-          const bottomValue = Number(firstLiElement.getBoundingClientRect().bottom.toFixed(0))
-          const newPosition = { ...position, top: topValue, bottom: bottomValue }
-          setPosition(newPosition)
-        } else {
-          const topValue = Number(labelElement.getBoundingClientRect().top.toFixed(0))
-          const bottomValue = Number(labelElement.getBoundingClientRect().bottom.toFixed(0))
-          const newPosition = { ...position, top: topValue, bottom: bottomValue }
-          setPosition(newPosition)
-        }
+type DefaultBackgroundType = ListElement[] | []
+
+const useDefaultBackground = (activeLi: DefaultBackgroundType) => {
+  const { toogleMobileNav, navBarMobilePosition, mobileNavLabelRef } = useContext(NavBarContext)
+  const { displayMenu } = toogleMobileNav
+  const { navBarItems } = useContext(NavBarContext)
+  const { position, setPosition } = navBarMobilePosition
+
+  useEffect(() => {
+    const labelElement = mobileNavLabelRef?.current!
+    const getActiveSection = navBarItems.listElements.filter(x => x.active === true)
+    if (displayMenu) {
+      if (getActiveSection.length > 0) {
+        const activeSectionName = `mobile_navbar_li_${getActiveSection[0].section}`
+        const firstLiElement = document.getElementById(activeSectionName)!;
+
+        const topValue = Number(firstLiElement.getBoundingClientRect().top.toFixed(0))
+        const bottomValue = Number(firstLiElement.getBoundingClientRect().bottom.toFixed(0))
+        const newPosition = { ...position, top: topValue, bottom: bottomValue }
+        setPosition(newPosition)
+      } else {
+        const topValue = Number(labelElement.getBoundingClientRect().top.toFixed(0))
+        const bottomValue = Number(labelElement.getBoundingClientRect().bottom.toFixed(0))
+        const newPosition = { ...position, top: topValue, bottom: bottomValue }
+        setPosition(newPosition)
       }
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [displayMenu, navBarItems.listElements, setPosition])
+  }, [displayMenu, navBarItems.listElements, setPosition])
 }
 
 export default useDefaultBackground
