1f1a4da9290116bb497f2dc0ccdaffc7a61cd91e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-23 20:09:26 +0200

    Commit sepuku

--- src/app/features/about/About.tsx
+++ src/app/features/about/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <div
         id="About"
         ref={aboutSectionRef}
-        className="relative h-full bg-white border-t-[1px] border-my-lightGray ">About</div>
+        className="relative h-full border-t-[1px] border-my-lightGray bg-gradient-to-br from-my-white from-20% via-my-lightBackground">About</div>
   )
 }
 
