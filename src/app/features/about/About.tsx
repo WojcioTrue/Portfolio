3fff3a77dbcfca8aa72a85fd1f69985f03e1d48c
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-25 19:15:53 +0200

    Created carousel

--- src/app/features/about/About.tsx
+++ src/app/features/about/About.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { NavBarContext } from '../navbar/navBarContext/NavBarContextProvider'
 import Header from './Header'
 import Description from './Description'
+import Carousel from './carousel/Carousel'
 
 
 const About = () => {
@@ -12,7 +13,8 @@ const About = () => {
       ref={aboutSectionRef}
       className="relative min-h-[calc(100%_-_77px)] border-t-[1px] border-my-lightGray bg-gradient-to-tl from-my-lightBackground from-[20%] via-my-white pt-10 pb-16 flex items-center flex-col justify-center">
       <Header />
-      <Description/>
+      <Description />
+      <Carousel />
     </div>
   )
 }
