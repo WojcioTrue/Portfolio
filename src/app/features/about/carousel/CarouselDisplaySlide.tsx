45f4efecf8589346d2b40c8e2145d1ae2943865f
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-27 19:13:11 +0200

    Created carousel display component

---
+++ src/app/features/about/carousel/CarouselDisplaySlide.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CarouselDisplaySlide = () => {
+  return (
+    <div className='bg-red-700 w-[100px] h-[200px]'>CarouselDisplaySlide</div>
+  )
+}
+
+export default CarouselDisplaySlide
--- src/app/features/about/carousel/CarouselWrapper.tsx
+++ src/app/features/about/carousel/CarouselWrapper.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CarouselDisplaySlide from './CarouselDisplaySlide'
 
 const CarouselWrapper = () => {
   return (
-    <div className='bg-green-600 w-[100%] max-w-[1200px] overflow-hidden h-[100px] '>CarouselWrapper</div>
+    <div className='bg-green-600 w-[100%] max-w-[1200px] overflow-hidden h-[100px] '>
+      <CarouselDisplaySlide/>
+    </div>
   )
 }
 
