fa8b61a3f9f7fccb603dfd027c0b3c9ea1d3f9d4
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-27 19:07:35 +0200

    Styled CarouselWrapper

--- src/app/features/about/carousel/CarouselWrapper.tsx
+++ src/app/features/about/carousel/CarouselWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CarouselWrapper = () => {
   return (
-    <div className='bg-green-600 w-[100%]'>CarouselWrapper</div>
+    <div className='bg-green-600 w-[100%] max-w-[1200px] overflow-hidden h-[100px] '>CarouselWrapper</div>
   )
 }
 
