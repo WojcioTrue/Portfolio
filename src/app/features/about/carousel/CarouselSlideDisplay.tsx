a5cdd7003957ba51e511573d5e0d44aeb69e8c48
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-28 12:18:25 +0200

    Created second slide

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -5,6 +5,7 @@ const CarouselSlideDisplay = () => {
   return (
     <div className='bg-red-700 w-[200px] h-[200px] relative'>
       <Slide/>
+      <Slide/>
     </div>
   )
 }
--- src/app/features/about/carousel/slide/Slide.tsx
+++ src/app/features/about/carousel/slide/Slide.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Slide = () => {
   return (
-    <div className='absolute bg-blue-500'>Slide</div>
+    <div className='absolute bg-blue-500 h-[100%] w-[100%]'>Slide</div>
   )
 }
 
