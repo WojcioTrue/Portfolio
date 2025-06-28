b6f5f9e4bb1f3027131db76136029e3a58c751cc
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-28 11:38:15 +0200

    Temporary styling

--- src/app/features/about/carousel/CarouselWrapper.tsx
+++ src/app/features/about/carousel/CarouselWrapper.tsx
@@ -3,10 +3,10 @@ import CarouselSlideDisplay from './CarouselSlideDisplay'
 
 const CarouselWrapper = () => {
   return (
-    <div className='flex flex-row justify-center bg-green-600 w-[100%] max-w-[1200px] overflow-hidden '>
+    <div className='relative flex flex-row justify-center bg-green-600 w-[100%] max-w-[1200px] overflow-hidden '>
       <CarouselSlideDisplay/>
-      <div className='bg-white h-[50px] w-[50px] absolute left-0'>Left</div>
-      <div className='bg-white h-[50px] w-[50px] absolute right-0'>right</div>
+      <div className='bg-white h-[50px] w-[50px] absolute left-0 top-1/2'>Left</div>
+      <div className='bg-white h-[50px] w-[50px] absolute right-0 top-1/2'>right</div>
     </div>
   )
 }
