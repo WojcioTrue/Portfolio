f50295580fdc6cf263e978c41fe38fcf2dd33633
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-28 12:13:30 +0200

    Created slide

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Slide from './slide/Slide'
 
 const CarouselSlideDisplay = () => {
   return (
-    <div className='bg-red-700 w-[200px] h-[200px]'>display slide</div>
+    <div className='bg-red-700 w-[200px] h-[200px] relative'>
+      <Slide/>
+    </div>
   )
 }
 
---
+++ src/app/features/about/carousel/slide/Slide.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slide = () => {
+  return (
+    <div className='absolute bg-blue-500'>Slide</div>
+  )
+}
+
+export default Slide
