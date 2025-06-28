76455a95ed29d95e5fc4401262bccf86f5c91ce2
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-28 13:52:11 +0200

    Passed array as argument to component

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -1,20 +1,13 @@
+import { SlideArrayType } from './CarouselWrapper'
 import Slide from './slide/Slide'
 
-const slideArray = [
-  {
-    text: 'first',
-    x: 0
-  },
-  {
-    text: 'second',
-    x: 250
-  }
-]
+const CarouselSlideDisplay = ({arr} : {arr: SlideArrayType[]}) => {
 
-const CarouselSlideDisplay = () => {
+
+
   return (
     <div className='bg-red-700 w-[200px] h-[200px] relative'>
-      {slideArray.map((x,i) => <Slide key={i} text={x.text} x={x.x}/>)}
+      {arr.map((x,i) => <Slide key={i} text={x.text} x={x.x}/>)}
     </div>
   )
 }
--- src/app/features/about/carousel/CarouselWrapper.tsx
+++ src/app/features/about/carousel/CarouselWrapper.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CarouselSlideDisplay from './CarouselSlideDisplay'
 
+export type SlideArrayType = {
+  text: string,
+  x: number
+}
+
+const slideArray: SlideArrayType[] = [
+  {
+    text: 'first',
+    x: 0
+  },
+  {
+    text: 'second',
+    x: 250
+  },
+  {
+    text: 'third',
+    x: 500
+  }
+
+]
+
 const CarouselWrapper = () => {
+  const [elArr, setElArr] = useState<SlideArrayType[]>(slideArray)
+
   return (
     <div className='relative flex flex-row justify-center bg-green-600 w-[100%] max-w-[1200px] overflow-hidden '>
-      <CarouselSlideDisplay/>
+      <CarouselSlideDisplay arr={elArr} />
       <div className='bg-white h-[50px] w-[50px] absolute left-0 top-1/2'>Left</div>
       <div className='bg-white h-[50px] w-[50px] absolute right-0 top-1/2'>right</div>
     </div>
