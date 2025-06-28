c0f9307aeb9e730addc7b5031827db3eb1330f91
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-28 11:27:33 +0200

    Styled arrows

--- src/app/features/about/carousel/CarouselWrapper.tsx
+++ src/app/features/about/carousel/CarouselWrapper.tsx
@@ -5,8 +5,8 @@ const CarouselWrapper = () => {
   return (
     <div className='flex flex-row justify-center bg-green-600 w-[100%] max-w-[1200px] overflow-hidden '>
       <CarouselDisplaySlide/>
-      <div className='bg-white h-[50px] w-[50px]'>Left</div>
-      <div className='bg-white h-[50px] w-[50px]'>Left</div>
+      <div className='bg-white h-[50px] w-[50px] absolute left-0'>Left</div>
+      <div className='bg-white h-[50px] w-[50px] absolute right-0'>right</div>
     </div>
   )
 }
