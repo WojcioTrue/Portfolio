5018e7e39dc7ce16005387b98de6128f77083497
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-06-29 17:54:31 +0200

    Encapsulated buttons, and styled

--- src/app/features/about/carousel/CarouselButton.tsx
+++ src/app/features/about/carousel/CarouselButton.tsx
@@ -12,8 +12,8 @@ const CarouselButton = ({ buttonClick, disabled, text }: CarouselButtonType) => 
             className='h-[50px] w-[50px] absolute top-1/2'
             style={{
                 backgroundColor: disabled ? 'red' : 'white',
-                left : text === "Left" ? '0' : undefined,
-                right : text === "Right" ? '0' : undefined
+                left: text === "Left" ? '0' : undefined,
+                right: text === "Right" ? '0' : undefined
             }}
         >{text}
         </button>
