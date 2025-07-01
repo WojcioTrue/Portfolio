eaa1d0bb8d2598e141805dfa5d34358ab245b7ab
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-01 22:08:11 +0200

    Changed prop name

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -1,15 +1,20 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { CarouselContext } from '../carouselContext/CarouselContextProvider'
 import Slide from './slide/Slide'
 
 const CarouselSlideDisplay = () => {
   const { carouseElementsArr, carouselSlideDisplayRef } = useContext(CarouselContext)
   const { array } = carouseElementsArr
+  
+  useEffect(() => {
+    console.log(carouselSlideDisplayRef?.current?.clientWidth)
+  },[carouselSlideDisplayRef])
+
   return (
     <div 
     ref={carouselSlideDisplayRef}
     className='bg-red-700 w-[200px] h-[200px] relative'>
-      {array.map((x, i) => <Slide key={i} text={x.text} x={x.x} index={i} opacity={x.visible ? 1 : 0} active={x.active} />)}
+      {array.map((x, i) => <Slide key={i} text={x.text} index={i} opacity={x.visible ? 1 : 0} active={x.active} gap={x.x} />)}
     </div>
   )
 }
--- src/app/features/about/carousel/slide/Slide.tsx
+++ src/app/features/about/carousel/slide/Slide.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 type SlideType = {
   text: string,
-  x: number,
+  gap: number,
   index: number,
   opacity: number,
   active: boolean
 }
 
-const Slide = ({ text, x, opacity, index, active }: SlideType) => {
+const Slide = ({ text, gap, opacity, index, active }: SlideType) => {
 
   return (
-    <div className='absolute h-[100%] w-[100%]'>
+    <div className='h-[100%] w-[100%] absolute'>
       <div className='bg-yellow-500 m-[10px] w-[90%] h-[90%]'
         style={{
-          transform: `translateX(${x}px) translateY(${x / 20}px)`,
+          transform: `translateX(${gap}px) translateY(${15}px)`,
           opacity: opacity
         }}>{text} {`${active}`}
       </div>
