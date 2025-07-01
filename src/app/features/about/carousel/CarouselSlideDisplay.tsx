4390f2734dd15c7abafdbb13596ad4392395c523
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-01 22:21:51 +0200

    Styled Slide component

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -9,21 +9,33 @@ const CarouselSlideDisplay = () => {
   const [alteredArr, setAlteredArr] = useState<SlideArrayType | undefined>(undefined)
 
   useEffect(() => {
-    const displayWidth = carouselSlideDisplayRef?.current?.getBoundingClientRect().width!
-    const newArr = array.map((x, i) => {
-      return {
-        ...x,
-        x: displayWidth * (i),
-      }
-    })
-    console.log(newArr)
+    const displayWidth = carouselSlideDisplayRef?.current?.getBoundingClientRect().width
+    if (displayWidth !== undefined) {
+      const newArr = array.map((x, i) => {
+        return {
+          ...x,
+          x: displayWidth * (i),
+        }
+      })
+      console.log(newArr)
+    }
+
   }, [array, carouselSlideDisplayRef])
 
   return (
     <div
       ref={carouselSlideDisplayRef}
       className='bg-red-700 w-[200px] h-[200px] relative'>
-      {array.map((x, i) => <Slide key={i} text={x.text} index={i} opacity={x.visible ? 1 : 0} active={x.active} gap={x.x} />)}
+      {array.map((x, i) =>
+        <Slide
+          key={i}
+          text={x.text}
+          x={x.x}
+          index={i}
+          opacity={x.visible ? 1 : 0}
+          active={x.active}
+          gap={x.gap}
+        />)}
     </div>
   )
 }
--- src/app/features/about/carousel/slide/Slide.tsx
+++ src/app/features/about/carousel/slide/Slide.tsx
@@ -2,19 +2,20 @@ import React from 'react'
 
 type SlideType = {
   text: string,
+  x: number,
   gap: number,
   index: number,
   opacity: number,
   active: boolean
 }
 
-const Slide = ({ text, gap, opacity, index, active }: SlideType) => {
+const Slide = ({ text, x, gap, opacity, index, active }: SlideType) => {
 
   return (
     <div className='h-[100%] w-[100%] absolute'>
       <div className='bg-yellow-500 m-[10px] w-[90%] h-[90%]'
         style={{
-          transform: `translateX(${gap}px) translateY(${15}px)`,
+          transform: `translateX(${x}px) translateY(${15}px)`,
           opacity: opacity
         }}>{text} {`${active}`}
       </div>
