46c0164709117009c09f95a9222a9c35a3317c42
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-01 21:49:07 +0200

    assigned reference to Carousel slide display div

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -3,10 +3,12 @@ import { CarouselContext } from '../carouselContext/CarouselContextProvider'
 import Slide from './slide/Slide'
 
 const CarouselSlideDisplay = () => {
-  const { carouseElementsArr } = useContext(CarouselContext)
+  const { carouseElementsArr, carouselSlideDisplayRef } = useContext(CarouselContext)
   const { array } = carouseElementsArr
   return (
-    <div className='bg-red-700 w-[200px] h-[200px] relative'>
+    <div 
+    ref={carouselSlideDisplayRef}
+    className='bg-red-700 w-[200px] h-[200px] relative'>
       {array.map((x, i) => <Slide key={i} text={x.text} x={x.x} index={i} opacity={x.visible ? 1 : 0} active={x.active} />)}
     </div>
   )
