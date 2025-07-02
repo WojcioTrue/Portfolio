4f1ef5a792ab6aefebc5bc99c72c78cd8ff1f1ff
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-02 22:50:28 +0200

    Modified array of elements

--- src/app/features/about/carouselContext/slidesArray.tsx
+++ src/app/features/about/carouselContext/slidesArray.tsx
@@ -1,37 +1,41 @@
 export type SlideArrayType = {
-    index: number,
-    text: string,
-    x: number,
+    width: number,
     gap: number,
-    visible: boolean,
-    active: boolean
+    elements: {
+        index: number,
+        text: string,
+        x: number,
+        visible: boolean,
+        active: boolean
+    }[]
 }
 
 
-export const slideArray: SlideArrayType[] = [
-    {
-        index: 0,
-        text: 'first',
-        x: 0,
-        gap: 0,
-        visible: true,
-        active: true,
-    },
-    {
-        index: 1,
-        text: 'second',
-        x: 0,
-        gap: 0,
-        visible: true,
-        active: false,
+export const slideArray: SlideArrayType = {
+    width: 0,
+    gap: 0,
+    elements: [
+        {
+            index: 0,
+            text: 'first',
+            x: 0,
+            visible: true,
+            active: true,
+        },
+        {
+            index: 1,
+            text: 'second',
+            x: 0,
+            visible: true,
+            active: false,
 
-    },
-    {
-        index: 2,
-        text: 'third',
-        x: 0,
-        gap: 0,
-        visible: true,
-        active: false,
-    }
-]
+        },
+        {
+            index: 2,
+            text: 'third',
+            x: 0,
+            visible: true,
+            active: false,
+        }
+    ]
+}
