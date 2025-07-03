4e432ac717840a4fb4276d85d93273fd1aecb4a1
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-03 20:26:25 +0200

    Bug

--- src/app/features/about/carouselHooks/rightClick/useRightClick.tsx
+++ src/app/features/about/carouselHooks/rightClick/useRightClick.tsx
@@ -1,46 +1,33 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext} from "react"
 import { CarouselContext } from "../../carouselContext/CarouselContextProvider"
 import { SlideArrayType } from "../../carouselContext/slidesArray"
-// import { SlideArrayType } from "../../carouselContext/slidesArray"
 
 const useCarouselRightClick = () => {
     const { carouseElementsArr } = useContext(CarouselContext)
     const { array, setArray } = carouseElementsArr
-    const [enableButton, setEnableButton] = useState<boolean>(false)
 
-    // move to button component
-    useEffect(() => {
-        if (array.width !== 0) {
-            setEnableButton(true)
-            console.log(array)
-        } else {
-            console.log(array)
-        }
-    }, [array])
-
-    const rightClickEffect = (arg : SlideArrayType) => {
-            const modifiedX = arg.elements.map((el) => {
-                if (el.x <= 0) {
-                    return {
-                        ...el,
-                        x: el.x - array.width,
-                        visible: false,
-                    }
-                } else {
-                    return {
-                        ...el,
-                        x: el.x - array.width
-                    }
+    const rightClickEffect = (arg: SlideArrayType) => {
+        const modifiedX = arg.elements.map((el) => {
+            if (el.x <= 0) {
+                return {
+                    ...el,
+                    x: el.x - array.width,
+                    visible: false,
                 }
-            })
-            const newArr = {
-                ...array,
-                elements: [
-                    ...modifiedX,
-                ]
+            } else {
+                return {
+                    ...el,
+                    x: el.x - array.width
+                }
             }
-            return newArr
-        
+        })
+        const newArr = {
+            ...array,
+            elements: [
+                ...modifiedX,
+            ]
+        }
+        return newArr
     }
 
     const modifyArrRight = () => {
