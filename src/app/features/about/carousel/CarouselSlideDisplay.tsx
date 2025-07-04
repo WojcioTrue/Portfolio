40eb050a2b9d9839c88eb603e365a004848718e9
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-04 19:57:22 +0200

    Display Slides when initial width element is set

--- src/app/features/about/carousel/CarouselSlideDisplay.tsx
+++ src/app/features/about/carousel/CarouselSlideDisplay.tsx
@@ -5,36 +5,15 @@ import useInitialWidth from '../carouselHooks/useInitialWidth'
 
 const CarouselSlideDisplay = () => {
   const { carouseElementsArr, carouselSlideDisplayRef } = useContext(CarouselContext)
-  const { array, setArray } = carouseElementsArr
+  const { array } = carouseElementsArr
   const [display, setDisplay] = useState(false)
   useInitialWidth()
 
-  
-  // initial width of slide Display element
   useEffect(() => {
-    const displayWidth = carouselSlideDisplayRef?.current?.getBoundingClientRect().width
-    if (displayWidth !== undefined) {
-      const newArr = array.elements.map((x,i) => {
-        return {
-          ...x,
-          x: displayWidth * (i * 1)
-        }
-      })
-
-      const modifiedArr = {
-        width: displayWidth,
-        gap: 20,
-        elements: [
-          ...newArr
-        ]
-      }
-      // if display is endabled, then width is updated 
+    if (array.width !== 0) {
       setDisplay(true)
-      setArray(modifiedArr)
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [setArray, carouselSlideDisplayRef])
+  }, [array])
 
   return (
     <div
