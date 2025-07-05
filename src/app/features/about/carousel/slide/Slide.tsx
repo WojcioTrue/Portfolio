346130d19c2f27398af7ad6bfcaecd8460230f12
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-07-05 15:49:28 +0200

    Moved styles to Upper slide element

--- src/app/features/about/carousel/slide/Slide.tsx
+++ src/app/features/about/carousel/slide/Slide.tsx
@@ -12,19 +12,20 @@ const Slide = ({ text, x, gap, index, active }: SlideType) => {
 
   const countOpacity = (arg: number) => {
     console.log('triggered')
-    if(0 > arg){
+    if (0 > arg) {
       return 0
     }
     return 1 - index * 0.3
   }
 
   return (
-    <div className='h-[100%] w-[100%] absolute flex items-center justify-center'>
+    <div className='h-[100%] w-[100%] absolute flex items-center justify-center'
+      style={{
+        transform: `translateX(${x + (gap * index)}px)`,
+        opacity: countOpacity(index)
+      }}>
       <div className='bg-yellow-500 w-[100%] h-[100%]'
-        style={{
-          transform: `translateX(${x + (gap * index)}px)`,
-          opacity: countOpacity(index)
-        }}>{text} {`${active}`}
+      >{text} {`${active}`}
       </div>
     </div>
 
