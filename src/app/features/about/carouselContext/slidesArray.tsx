6be10c6c419c00e5760b0321392270581d545343
Wojciech.Otawski <wojciech.otawski@gmail.com>
2025-08-02 15:40:35 +0200

    Added undefined value for href and liveView

--- src/app/features/about/carouselContext/slidesArray.tsx
+++ src/app/features/about/carouselContext/slidesArray.tsx
@@ -7,8 +7,8 @@ export type SlideArrayType = {
     index: number;
     header: string;
     description: string;
-    href: string;
-    liveView: string;
+    href: string | undefined;
+    liveView: string | undefined;
     visible: boolean;
     opacity: number;
     active: boolean;
@@ -17,63 +17,7 @@ export type SlideArrayType = {
 
 export const slideArray: SlideArrayType = {
   height: 0,
-  elements: [
-    {
-      key: uuidv4(),
-      index: 0,
-      header: "Header 1",
-      description: "Description 1",
-      href: "href 1",
-      liveView: "liveView 1",
-      visible: true,
-      opacity: 0,
-      active: true,
-    },
-    {
-      key: uuidv4(),
-      index: 1,
-      header: "Header 2",
-      description: "Description 2",
-      href: "href 2",
-      liveView: "liveView 2",
-      visible: true,
-      opacity: 0,
-      active: false,
-    },
-    {
-      key: uuidv4(),
-      index: 2,
-      header: "Header 3",
-      description: "Description 3",
-      href: "href 3",
-      liveView: "liveView 3",
-      visible: true,
-      opacity: 0,
-      active: false,
-    },
-    {
-      key: uuidv4(),
-      index: 3,
-      header: "Header 4",
-      description: "Description 4",
-      href: "href 4",
-      liveView: "liveView 4",
-      visible: true,
-      opacity: 0,
-      active: false,
-    },
-    {
-      key: uuidv4(),
-      index: 4,
-      header: "Header 5",
-      description: "Description 5",
-      href: "href 5",
-      liveView: "liveView 5",
-      visible: true,
-      opacity: 0,
-      active: false,
-    },
-  ],
+  elements: [],
 };
 
 export const emptyArr: SlideArrayType = {
@@ -84,9 +28,9 @@ export const emptyArr: SlideArrayType = {
       index: 0,
       header: "OOOOppppSSSS!!!!!!",
       description:
-        "Looks like your slide Array is empty. Put something inside it!",
-      href: "#",
-      liveView: "#",
+        "Looks like your array for slides is empty. Put something inside it!",
+      href: undefined,
+      liveView: undefined,
       visible: true,
       opacity: 0,
       active: true,
